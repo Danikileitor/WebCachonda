8e990e6947842b023712632fb7467a3d8dc38259
Danikileitor <focusyi@hotmail.com>
2022-10-10 12:56:23 +0200

    Mejoramos el ejercicio

--- DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R6_Ej1_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R6_Ej1_Daniel.java
@@ -3,7 +3,7 @@ import java.util.Scanner; //Importación de paquetes
 public class U2_R6_Ej1_Daniel{
 	public static void main(String []args){
 	//Declarar variables
-	int numero1, numero2, numero3;
+	int numero1, numero2, numero3, auxiliar, auxiliar2;
 	Scanner teclado=new Scanner(System.in);
 	//Desarrollo del programa
 	System.out.println("Dime tres números enteros y te los muestro ordenados de mayor a menor");
@@ -17,32 +17,26 @@ public class U2_R6_Ej1_Daniel{
 		System.out.println("Los números "+numero1+", "+numero2+" y "+numero3+" son iguales.");//Indicar que los números son iguales
 	}
 	else{
-		if(numero1>numero2){
-			if(numero1>numero3){
-				if(numero2>numero3){
-					System.out.println(numero1+" > "+numero2+" > "+numero3);
-				}
-				else{
-					System.out.println(numero1+" > "+numero3+" > "+numero2);
-				}
-			}
-			else{
-				System.out.println(numero3+" > "+numero1+" > "+numero2);
+		if(numero1<numero2){
+			auxiliar=numero1;
+			numero1=numero2;
+			numero2=auxiliar;		
+		}
+		if(numero3<numero1){
+			if(numero3>numero2){
+				auxiliar=numero2;
+				numero2=numero3;
+				numero3=auxiliar;
 			}
 		}
 		else{
-			if(numero2>numero3){
-				if(numero1>numero3){
-					System.out.println(numero2+" > "+numero1+" > "+numero3);
-				}
-				else{
-					System.out.println(numero2+" > "+numero3+" > "+numero1);
-				}
-			}
-			else{
-				System.out.println(numero3+" > "+numero2+" > "+numero1);
-			}
+				auxiliar=numero1;
+				numero1=numero3;
+				auxiliar2=numero2;
+				numero2=auxiliar;
+				numero3=auxiliar2;
 		}
+		System.out.println("Los números ordeados son: "+numero1+", "+numero2+" y "+numero3);//Mostrar los números ordenados
 	}
 	}//Fin main
 }//Fin clase
