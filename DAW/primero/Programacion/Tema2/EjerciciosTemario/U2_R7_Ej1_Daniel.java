1c251562bb7202ea749ce569b703f24d390d7d2a
Danikileitor <focusyi@hotmail.com>
2022-10-14 13:22:08 +0200

    edad

--- DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R7_Ej1_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R7_Ej1_Daniel.java
@@ -3,19 +3,79 @@ import java.util.Scanner; //Importación de paquetes
 public class U2_R7_Ej1_Daniel{
 	public static void main(String []args){
 	//Declarar variables
-	int edad = 10;
+	int edad;
+	Scanner teclado=new Scanner(System.in);
 	//Desarrollo del programa
-	switch (edad-2) {
+	System.out.println("Indique su edad:");
+	edad = teclado.nextInt();
+	/*Esto pa java 17
+	switch (edad) {
+		case 0, 1, 2 ->
+			System.out.println("Guarderia");
+		case 3, 4, 5 ->
+			System.out.println("Prescolar");
+		case 6, 7, 8, 9, 10, 11 ->
+			System.out.println("Primaria");
+		case 12, 13, 14, 15, 16 ->
+			System.out.println("Secundaria");
+		default ->
+			System.out.println("Estudios postobligatorios");
+	}*/
+	switch (edad) {
+		case 0:
+			System.out.println("Guarderia");
+			break;
 		case 1:
-			System.out.println("Hola ");
+			System.out.println("Guarderia");
+			break;
 		case 2:
-			System.out.println("Me alegro ");
+			System.out.println("Guarderia");
+			break;
+		case 3:
+			System.out.println("Prescolar");
+			break;
+		case 4:
+			System.out.println("Prescolar");
+			break;
+		case 5:
+			System.out.println("Prescolar");
+			break;
+		case 6:
+			System.out.println("Primaria");
+			break;
+		case 7:
+			System.out.println("Primaria");
+			break;
 		case 8:
-			System.out.println("de conocerte ");
+			System.out.println("Primaria");
+			break;
+		case 9:
+			System.out.println("Primaria");
+			break;
+		case 10:
+			System.out.println("Primaria");
+			break;
 		case 11:
-			System.out.println("hoy mismo ");
+			System.out.println("Primaria");
+			break;
+		case 12:
+			System.out.println("Secundaria");
+			break;
+		case 13:
+			System.out.println("Secundaria");
+			break;
+		case 14:
+			System.out.println("Secundaria");
+			break;
+		case 15:
+			System.out.println("Secundaria");
+			break;
+		case 16:
+			System.out.println("Secundaria");
+			break;
 		default:
-			System.out.println("Sin precedentes");
+			System.out.println("Estudios postobligatorios");
+			break;
 	}
 	}//Fin main
 }//Fin clase
