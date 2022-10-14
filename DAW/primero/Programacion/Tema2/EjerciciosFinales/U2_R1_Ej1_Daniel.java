256c7aed054c79c59ecb525bbcbc8e7b40eba16e
Danikileitor <focusyi@hotmail.com>
2022-10-14 14:06:08 +0200

    ej1

---
+++ DAW/primero/Programacion/Tema2/EjerciciosFinales/U2_R1_Ej1_Daniel.java
@@ -0,0 +1,17 @@
+//Daniel Marcos Guerra Gómez
+import java.util.Scanner; //Importación de paquetes
+public class U2_R1_Ej1_Daniel{
+	public static void main(String []args){
+	//Declarar variables
+	int c, estaEsUnaVariable, q76354, numero;
+	Scanner teclado=new Scanner(System.in);
+	//Desarrollo del programa
+	System.out.println("Introduzca un número:");
+	int valor = teclado.nextInt();
+	System.out.println("Este es un programa en Java");
+	System.out.printf("Este es un%s","programa en Java");
+	if(numero!=7){
+		System.out.println("La variable numero no es igual a 7");
+	}//No compila porque no se ha inicializado la variable numero ya que no se pide
+	}//Fin main
+}//Fin clase
