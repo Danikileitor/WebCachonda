3e2f7b3e1a6c527e5d66485465e6b613f96cb8f1
Danikileitor <focusyi@hotmail.com>
2022-10-14 12:28:38 +0200

    switch

---
+++ DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R7_Ej1_Daniel.java
@@ -0,0 +1,21 @@
+//Daniel Marcos Guerra Gómez
+import java.util.Scanner; //Importación de paquetes
+public class U2_R7_Ej1_Daniel{
+	public static void main(String []args){
+	//Declarar variables
+	int edad = 10;
+	//Desarrollo del programa
+	switch (edad) {
+		case 1:
+			System.out.println("Hola ");
+		case 2:
+			System.out.println("Me alegro ");
+		case 8:
+			System.out.println("de conocerte ");
+		case 11:
+			System.out.println("hoy mismo ");
+		default:
+			System.out.println("Sin precedentes");
+	}
+	}//Fin main
+}//Fin clase
