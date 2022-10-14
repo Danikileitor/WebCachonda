40ea784a46c1194cde4c80ad587778e9677c2f88
Danikileitor <focusyi@hotmail.com>
2022-10-14 13:28:59 +0200

    hackedad

--- DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R8_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosTemario/U2_R8_Daniel.java
@@ -23,53 +23,27 @@ public class U2_R8_Daniel{
 	}*/
 	switch (edad) {
 		case 0:
-			System.out.println("Guarderia");
-			break;
 		case 1:
-			System.out.println("Guarderia");
-			break;
 		case 2:
 			System.out.println("Guarderia");
 			break;
 		case 3:
-			System.out.println("Prescolar");
-			break;
 		case 4:
-			System.out.println("Prescolar");
-			break;
 		case 5:
 			System.out.println("Prescolar");
 			break;
 		case 6:
-			System.out.println("Primaria");
-			break;
 		case 7:
-			System.out.println("Primaria");
-			break;
 		case 8:
-			System.out.println("Primaria");
-			break;
 		case 9:
-			System.out.println("Primaria");
-			break;
 		case 10:
-			System.out.println("Primaria");
-			break;
 		case 11:
 			System.out.println("Primaria");
 			break;
 		case 12:
-			System.out.println("Secundaria");
-			break;
 		case 13:
-			System.out.println("Secundaria");
-			break;
 		case 14:
-			System.out.println("Secundaria");
-			break;
 		case 15:
-			System.out.println("Secundaria");
-			break;
 		case 16:
 			System.out.println("Secundaria");
 			break;
