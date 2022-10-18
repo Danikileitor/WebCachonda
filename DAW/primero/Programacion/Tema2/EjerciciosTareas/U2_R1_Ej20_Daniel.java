d557069d8f072de8ba5f2aa9e6e5ca3aa0b5e599
Danikileitor <focusyi@hotmail.com>
2022-10-18 13:03:53 +0200

    Ejercicios 11 a 20

--- DAW/primero/Programacion/Tema2/EjerciciosTareas/U2_R1_Ej20_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosTareas/U2_R1_Ej20_Daniel.java
@@ -3,11 +3,11 @@ import java.util.Scanner; //Importación de paquetes
 public class U2_R1_Ej20_Daniel{
 	public static void main(String []args){
 	//Declarar variables
-	int numero,decenaDeMillar,millar,centena,decena,unidad;
+	int numero;
 	Scanner teclado=new Scanner(System.in);
 	//Desarrollo del programa
 	System.out.println("Introduce un número entero de 5 dígitos:");
 	numero = teclado.nextInt();
-	System.out.printf("%1d   %1d   %1d   %1d   %1d",numero/10000,numero/1000,numero/100,numero/10,numero%10);
+	System.out.printf("%d   %d   %d   %d   %d",numero/10000,(numero%10000)/1000,((numero%10000)%1000)/100,(((numero%10000)%1000)%100)/10,((((numero%10000)%1000)%100)%10));
 	}//Fin main
 }//Fin clase
