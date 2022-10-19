cc9cc977eba5acd300696b5df7d59ff952207788
Danikileitor <focusyi@hotmail.com>
2022-10-19 12:18:49 +0200

    Se pudo el 16

---
+++ DAW/primero/Programacion/Tema2/EjerciciosFinales/U2_R2_Ej16_Daniel.java
@@ -0,0 +1,14 @@
+//Daniel Marcos Guerra Gómez
+import java.util.Scanner; //Importación de paquetes
+public class U2_R2_Ej16_Daniel{
+	public static void main(String []args){
+	//Declarar variables
+	int numero;
+	Scanner teclado=new Scanner(System.in);
+	//Desarrollo del programa
+	System.out.println("Introduzca un número y calcularé su valor absoluto:");
+	numero=teclado.nextInt();
+	numero=numero<0 ? numero*-1 : numero;
+	System.out.println("El valor absoluto es: "+numero);
+	}//Fin main
+}//Fin clase
