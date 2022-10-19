facfce1a47e0a42fd69e30691f0949fcfe164a28
Danikileitor <focusyi@hotmail.com>
2022-10-19 10:09:55 +0200

    Se pudo el 15

--- DAW/primero/Programacion/Tema2/EjerciciosFinales/U2_R2_Ej15_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosFinales/U2_R2_Ej15_Daniel.java
@@ -3,7 +3,7 @@ import java.util.Scanner; //Importación de paquetes
 public class U2_R2_Ej15_Daniel{
 	public static void main(String []args){
 	//Declarar variables
-	double base,altura,area;
+	double base,altura;
 	Scanner teclado=new Scanner(System.in);
 	//Desarrollo del programa
 	System.out.println("Vamos a calcular el área de un triángulo.\nDime cuanto mide su base:");
@@ -14,8 +14,7 @@ public class U2_R2_Ej15_Daniel{
 		System.out.println("No tiene sentido que indiques números negativos!");
 	}
 	else{
-		area=base*altura/2;
-		System.out.println("El área del triángulo con base "+base+" y altura "+altura+" es: "+area);
+		System.out.println("El área del triángulo con base "+base+" y altura "+altura+" es: "+base*altura/2);
 	}
 	}//Fin main
 }//Fin clase
