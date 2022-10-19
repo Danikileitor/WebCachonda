3d1550262ac4db8f4be35991d3f72f49cc9e3a7d
Danikileitor <focusyi@hotmail.com>
2022-10-19 09:47:19 +0200

    Ejercicios 11 a 20

--- DAW/primero/Programacion/Tema2/EjerciciosTareas/U2_R1_Ej19_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosTareas/U2_R1_Ej19_Daniel.java
@@ -1,21 +1,7 @@
 //Daniel Marcos Guerra Gómez
 public class U2_R1_Ej19_Daniel{
 	public static void main(String []args){
-	//Declarar variables	
-	char aMayuscula = 'A';
-	char aMinuscula = 'a';
-	char bMayuscula = 'B';
-	char bMinuscula = 'b';
-	char cMayuscula = 'C';
-	char cMinuscula = 'c';
-	char cero = '0';
-	char uno = '1';
-	char dos = '2';
-	char dolar = '$';
-	char asterisco = '*';
-	char barra = '/';
-	char blanco = ' ';
 	//Desarrollo del programa
-	System.out.printf("%s %s %s %s %s %s %s %s %s %s %s %s %s",(int) aMayuscula,(int) bMayuscula,(int) cMayuscula,(int) aMinuscula,(int) bMinuscula,(int) cMinuscula,(int) cero,(int) uno,(int) dos,(int) dolar,(int) asterisco,(int) barra,(int) blanco);
+	System.out.printf("%s %s %s %s %s %s %s %s %s %s %s %s %s",(int) 'A',(int) 'B',(int) 'C',(int) 'a',(int) 'b',(int) 'c',(int) '0',(int) '1',(int) '2',(int) '$',(int) '*',(int) '/',(int) ' ');
 	}//Fin main
 }//Fin clase
