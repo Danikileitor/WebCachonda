658d5060a6175c8ef4ff9628544b68e1f6098b69
Danikileitor <focusyi@hotmail.com>
2022-10-21 12:33:48 +0200

    No se si tengo bien el 19

--- DAW/primero/Programacion/Tema2/EjerciciosFinales/U2_R2_Ej19_Daniel.java
+++ DAW/primero/Programacion/Tema2/EjerciciosFinales/U2_R2_Ej19_Daniel.java
@@ -12,7 +12,7 @@ public class U2_R2_Ej19_Daniel{
 	//Desarrollo del programa
 	System.out.println("Dime los grados que tenga un ángulo y te los paso a radianes:");
 	grados=teclado.nextDouble();
-	grados=grados>=0 && grados<=360 ? grados : grados%360;//Comprobamos que "grados" se encunetra entre 0 y 360, en caso contrario lo transforma dentro de dicho rango.
+	grados=grados>=0 && grados<360 ? grados : grados%360;//Comprobamos que "grados" se encunetra entre 0 y 360, en caso contrario lo transforma dentro de dicho rango.
 	grados=grados*Math.PI/180;//pasamos a radianes
 	System.out.println("Su equivalente en radianes es: "+grados);
 	}//Fin main
