375d75213deddd75ce2146a65acbdf2b59c730bc
Danikileitor <focusyi@hotmail.com>
2022-11-09 14:43:17 +0100

    Ej15

---
+++ DAW/primero/Programacion/Tema3/EjerciciosTemario/U3_R1_Ej3_15_Daniel.java
@@ -0,0 +1,20 @@
+//Daniel Marcos Guerra Gómez
+/*Pedir por consola un número n y dibujar un triángulo rectángulo de n elementos de lado, utilizando para ello asteriscos (*). Por ejemplo, para n = 4:
+* * * *
+* * *
+* *
+*/
+import java.util.Scanner; //Importación de paquetes
+public class U3_R1_Ej3_15_Daniel{
+	public static void main(String []args){
+	//Declarar variables
+	int n;
+	Scanner teclado=new Scanner(System.in);
+	//Desarrollo del programa
+	System.out.println("¡Bienvenido al programa para dibujar un triángulo rectángulo!\nIndique su tamaño:");
+	n=teclado.nextInt();
+	for(){
+	}
+	System.out.println("¡Fin del programa!");
+	}//Fin main
+}//Fin clase
