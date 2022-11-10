28e708b39f6ac81cd8fe8aea1e26d8dde525ca1b
Danikileitor <focusyi@hotmail.com>
2022-11-10 16:53:01 +0100

    Ej7

---
+++ DAW/primero/Programacion/Tema3/EjerciciosTemario/U3_R1_Ej3_7_Daniel.java
@@ -0,0 +1,12 @@
+//Daniel Marcos Guerra Gómez
+/*Muestra todos los múltiplos de 7 menores de 100.*/
+public class U3_R1_Ej3_7_Daniel{
+	public static void main(String []args){
+	//Desarrollo del programa
+	System.out.println("Lista de múltiplos de 7 menores a 100:");
+	for(int i=0;i<100;i+=7){
+		System.out.println(i);
+	}
+	System.out.println("Fin del programa.");
+	}//Fin main
+}//Fin clase
