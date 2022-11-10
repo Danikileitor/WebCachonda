9c0931b3869f1eb461de9b5c8071da95f0d3cd43
Danikileitor <focusyi@hotmail.com>
2022-11-10 18:45:19 +0100

    Ej12

--- DAW/primero/Programacion/Tema3/EjerciciosTemario/U3_R1_Ej3_12_Daniel.java
+++ DAW/primero/Programacion/Tema3/EjerciciosTemario/U3_R1_Ej3_12_Daniel.java
@@ -4,13 +4,21 @@ import java.util.Scanner; //Importación de paquetes
 public class U3_R1_Ej3_12_Daniel{
 	public static void main(String []args){
 	//Declarar variables
-	boolean suspenso;
+	boolean suspenso=false;
+	byte nota;
 	Scanner teclado=new Scanner(System.in);
 	//Desarrollo del programa
 	System.out.println("Introduce las notas de 5 exámenes y te diré si hay algún suspenso.");
-	for(int i=1;i>=5;i++){
+	for(int i=1;i<=5;i++){
 		System.out.println("Introduce una nota del 0 al 10:");
+		nota=teclado.nextByte();
+		suspenso=nota>=5?suspenso:true;
 	}
-	System.out.printf("El factorial %d! es %d.%n",numero,factorial);//Usando long se puede llegar a calcular hasta el factorial de 25.
+	if(suspenso){
+		System.out.println("Hay algún suspenso.");
+	}
+	else{
+		System.out.println("No hay ningún suspenso.");
+	}
 	}//Fin main
 }//Fin clase
