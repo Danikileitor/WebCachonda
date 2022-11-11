e11f1d7a7bc83b2524a530300a3bedb3c70adf44
Danikileitor <focusyi@hotmail.com>
2022-11-11 13:42:28 +0100

    Ej11

--- DAW/primero/Programacion/Tema3/EjerciciosFinales/U3_R2_Ej11_Daniel.java
+++ DAW/primero/Programacion/Tema3/EjerciciosFinales/U3_R2_Ej11_Daniel.java
@@ -1,7 +1,7 @@
 //Daniel Marcos Guerra Gómez
 /*Realiza un programa que convierta un número decimal en su representación binaria. Hay que tener en cuenta que desconocemos cuántas cifras tiene el número que introduce el usuario. Por simplicidad, iremos mostrando el número binario con un dígito por línea.*/
 import java.util.Scanner; //Importación de paquetes
-public class U3_R2_Ej1_Daniel{
+public class U3_R2_Ej11_Daniel{
 	public static void main(String []args){
 	//Declarar variables
 	int numero, aux;
