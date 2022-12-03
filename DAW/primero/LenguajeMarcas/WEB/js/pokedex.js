60541da827b7e20ee8754a4840eb4811ea7381ad
Danikileitor <focusyi@hotmail.com>
2022-12-03 13:23:34 +0100

    WEB

--- DAW/primero/LenguajeMarcas/WEB/js/pokedex.js
+++ DAW/primero/LenguajeMarcas/WEB/js/pokedex.js
@@ -1,12 +1,12 @@
 function previous() {
     let pokemonId = document.pokemonKeySend.pokemonId.value
-    pokemonId = (Number(pokemonId) == 1) ? 905 : Number(pokemonId)-1; //cuando actualizen la api el límite será de 1010
+    pokemonId = (Number(pokemonId) == 1) ? 905 : Number(pokemonId) - 1; //cuando actualizen la api el límite será de 1010
     getPokemon(pokemonId)
 }
 
 function next() {
     let pokemonId = document.pokemonKeySend.pokemonId.value
-    pokemonId = (Number(pokemonId) == 905) ? 1 : Number(pokemonId)+1;
+    pokemonId = (Number(pokemonId) == 905) ? 1 : Number(pokemonId) + 1;
     getPokemon(pokemonId)
 }
 
@@ -19,38 +19,38 @@ function pokemonKeyId() {
 
 function marcar(idElemento) {
     document.getElementById(idElemento).checked = !document.getElementById(idElemento).checked;
-    if(document.getElementById(idElemento).checked){document.getElementById("bshiny").className += " shiny";}
-    else{document.getElementById("bshiny").className = "button";}
+    if (document.getElementById(idElemento).checked) { document.getElementById("bshiny").className += " shiny"; }
+    else { document.getElementById("bshiny").className = "button"; }
 }
 
 function vermasinfo() {
-    if(document.getElementById("masinfo").className=="oculto"){document.getElementById("masinfo").className="";}
-    else{document.getElementById("masinfo").className="oculto";}
+    if (document.getElementById("masinfo").className == "oculto") { document.getElementById("masinfo").className = ""; }
+    else { document.getElementById("masinfo").className = "oculto"; }
 }
 
 function getPokemon(id) {
-    typesJson = '{'+
-    '"bug": "img/tipos/bicho.png",'+
-    '"dark": "img/tipos/siniestro.png",'+
-    '"dragon": "img/tipos/dragon.png",'+
-    '"electric": "img/tipos/electrico.png",'+
-    '"fairy": "img/tipos/hada.png",'+
-    '"fighting": "img/tipos/lucha.png",'+
-    '"fire": "img/tipos/fuego.png",'+
-    '"flying": "img/tipos/volador.png",'+
-    '"ghost": "img/tipos/fantasma.png",'+
-    '"grass": "img/tipos/planta.png",'+
-    '"ground": "img/tipos/tierra.png",'+
-    '"ice": "img/tipos/hielo.png",'+
-    '"normal": "img/tipos/normal.png",'+
-    '"poison": "img/tipos/veneno.png",'+
-    '"psychic": "img/tipos/psiquico.png",'+
-    '"rock": "img/tipos/roca.png",'+
-    '"steel": "img/tipos/acero.png",'+
-    '"water": "img/tipos/agua.png"'+
-'}'
+    typesJson = '{' +
+        '"bug": "img/tipos/bicho.png",' +
+        '"dark": "img/tipos/siniestro.png",' +
+        '"dragon": "img/tipos/dragon.png",' +
+        '"electric": "img/tipos/electrico.png",' +
+        '"fairy": "img/tipos/hada.png",' +
+        '"fighting": "img/tipos/lucha.png",' +
+        '"fire": "img/tipos/fuego.png",' +
+        '"flying": "img/tipos/volador.png",' +
+        '"ghost": "img/tipos/fantasma.png",' +
+        '"grass": "img/tipos/planta.png",' +
+        '"ground": "img/tipos/tierra.png",' +
+        '"ice": "img/tipos/hielo.png",' +
+        '"normal": "img/tipos/normal.png",' +
+        '"poison": "img/tipos/veneno.png",' +
+        '"psychic": "img/tipos/psiquico.png",' +
+        '"rock": "img/tipos/roca.png",' +
+        '"steel": "img/tipos/acero.png",' +
+        '"water": "img/tipos/agua.png"' +
+        '}'
 
-    let pokemonApi = "https://pokeapi.co/api/v2/pokemon/"+id;
+    let pokemonApi = "https://pokeapi.co/api/v2/pokemon/" + id;
     // let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     let pokemonreq = new XMLHttpRequest();
     // let typesreq = new XMLHttpRequest();
@@ -60,19 +60,19 @@ function getPokemon(id) {
     document.getElementById("pokemonId").value = pokemonData.id;
     document.getElementById("name-display").innerHTML = pokemonData.name;
     // document.getElementById("imgPokemon").src = pokemonData.sprites.front_default;
-    if(document.getElementById("shiny").checked){document.getElementById("imgPokemon").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+pokemonData.id+".png";}
-    else{document.getElementById("imgPokemon").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokemonData.id+".png";}
+    if (document.getElementById("shiny").checked) { document.getElementById("imgPokemon").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pokemonData.id + ".png"; }
+    else { document.getElementById("imgPokemon").src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokemonData.id + ".png"; }
     document.getElementById("pokemonType").innerHTML = "";
     pokemonTypes = JSON.parse(typesJson);
-    pokemonData.types.map(item => document.getElementById("pokemonType").innerHTML += "<img src="+pokemonTypes[item.type.name]+" height=50>");
+    pokemonData.types.map(item => document.getElementById("pokemonType").innerHTML += "<img src=" + pokemonTypes[item.type.name] + " height=50>");
     // Ahora viene la parte de información adicional
-    let speciesApi = "https://pokeapi.co/api/v2/pokemon-species/"+pokemonData.species.name;
+    let speciesApi = "https://pokeapi.co/api/v2/pokemon-species/" + pokemonData.species.name;
     let speciesreq = new XMLHttpRequest();
     speciesreq.open("GET", speciesApi, false);
     speciesreq.send();
-    let speciesData = JSON.parse(pokemonreq.responseText);
-    //document.getElementById("categoria").value = speciesData.genera[0].genus.language;
-    document.getElementById("altura").value = pokemonData.height/10+" m";
-    document.getElementById("peso").value = pokemonData.weight/10+" kg";
+    let speciesData = JSON.parse(speciesreq.responseText);
+    document.getElementById("categoria").value = speciesData.genera[5].genus;
+    document.getElementById("altura").value = pokemonData.height / 10 + " m";
+    document.getElementById("peso").value = pokemonData.weight / 10 + " kg";
     document.getElementById("habilidad").value = pokemonData.abilities[0].ability.name;
 }
