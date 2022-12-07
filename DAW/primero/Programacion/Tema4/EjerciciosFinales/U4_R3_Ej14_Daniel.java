9179433ae213c5319f7389044bbcf8fbb302c28a
Danikileitor <focusyi@hotmail.com>
2022-12-07 13:15:56 +0100

    Ej14

--- DAW/primero/Programacion/Tema4/EjerciciosFinales/U4_R3_Ej14_Daniel.java
+++ DAW/primero/Programacion/Tema4/EjerciciosFinales/U4_R3_Ej14_Daniel.java
@@ -1,16 +1,24 @@
 //Daniel Marcos Guerra Gómez
-//Crea la función: 'muestraPares(int n)' que muestre por consola los primeros n números pares.
+//Escribe una función a la que se pase como parámetros de entrada una cantidad de días, horas y minutos. La función calculará y devolverá el número de segundos que existen en los datos de entrada.
 import java.util.Scanner; //Importación de paquetes
 
 public class U4_R3_Ej14_Daniel {
 	public static void main(String[] args) {
 		// Declarar variables
-		int cantidad;
+		int dias,horas,minutos;
 		Scanner teclado = new Scanner(System.in);
 		// Desarrollo del programa
-		System.out.println("Introduce un número entero y te mostrare esos primeros números pares:");
-		cantidad = teclado.nextInt();
-		System.out.printf("Los primeros %d números pares son:%n",cantidad);
-		muestraPares(1,cantidad);
+		System.out.println("Introduce una cantidad de días, horas y minutos para mostrarte la cantidad de segundos totales:");
+		System.out.println("Introduce los días:");
+		dias = teclado.nextInt();
+		System.out.println("Introduce las horas:");
+		horas = teclado.nextInt();
+		System.out.println("Introduce los minutos:");
+		minutos = teclado.nextInt();
+		System.out.printf("La cantidad de segundos que hay en %d días, %d horas y %d minutos es: %d.%n",dias,horas,minutos,calcularSegundos(dias, horas, minutos));
 	}// Fin main
+
+	public static int calcularSegundos(int d, int h, int m) {
+		return 0;
+	}
 }// Fin clase
