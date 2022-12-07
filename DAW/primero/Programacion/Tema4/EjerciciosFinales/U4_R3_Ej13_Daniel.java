4b972dfdc59b16e09a7d69ff51fc30c58e317969
Danikileitor <focusyi@hotmail.com>
2022-12-07 12:45:26 +0100

    Ej13

---
+++ DAW/primero/Programacion/Tema4/EjerciciosFinales/U4_R3_Ej13_Daniel.java
@@ -0,0 +1,24 @@
+//Daniel Marcos Guerra Gómez
+//Crea la función: 'muestraPares(int n)' que muestre por consola los primeros n números pares.
+import java.util.Scanner; //Importación de paquetes
+
+public class U4_R3_Ej12_Daniel {
+	public static void main(String[] args) {
+		// Declarar variables
+		int cantidad;
+		Scanner teclado = new Scanner(System.in);
+		// Desarrollo del programa
+		System.out.println("Introduce un número entero y te mostrare esos primeros números pares:");
+		cantidad = teclado.nextInt();
+		System.out.printf("Los primeros %d números pares son:%n",cantidad);
+		muestraPares(cantidad);
+	}// Fin main
+
+	public static void muestraPares(int n) {
+		for(int i=1;i<=n;i++){
+			if (i%2 == 0) {
+                System.out.println(i);
+            }
+		}
+	}
+}// Fin clase
