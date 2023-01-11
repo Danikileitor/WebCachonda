052505fcbf1fd4501aaf1c6a09a53fe2d76dc3f2
Danikileitor <focusyi@hotmail.com>
2023-01-11 14:20:01 +0100

    U6_R1_Ej2

--- DAW/primero/Programacion/Tema6/EjerciciosTemario/U6_R1_Ej2.java
+++ DAW/primero/Programacion/Tema6/EjerciciosTemario/U6_R1_Ej2.java
@@ -1,5 +1,6 @@
 //Daniel Marcos Guerra Gómez
-//Implementa un menú recursivo con confirmación de salida. Donde en una de las opciones generes números aleatorios (enteros positivos) y compruebes a que símbolo corresponde. Para ello debes usar la clase Character.
+//Introducir por teclado dos frases e indicar cuál de ellas es la más corta, es decir, la que contiene menos caracteres.
+//Va a ser el case 6
 import java.util.Scanner; //Importación de paquetes
 
 public class U6_R1_Ej2 {
@@ -51,6 +52,11 @@ public class U6_R1_Ej2 {
 							System.out.println(frase1 + frase2);
 							System.out.println(frase1.concat(frase2));
 							break;
+						case 6:
+							frase1 = pedirFrase();
+							frase2 = pedirFrase();
+							indicarLaMasCorta(frase1, frase2);
+							break;
 						default:
 							System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
 							break;
@@ -61,7 +67,7 @@ public class U6_R1_Ej2 {
 	}
 
 	public static void menu() {
-		System.out.printf("%n#################################################################################%n# Bienvenido a este maravilloso programa, elige una de las siguientes opciones: #%n# [1] Mostrar las String con un método                                          #%n# [2] Usar el método valueOf()                                                  #%n# [3] Usar el método equals()                                                   #%n# [4] Usar el método regionMatches()                                            #%n# [5] Concatenar cadenas usando concat()                                        #%n# [0] Salir                                                                     #%n#################################################################################%n");
+		System.out.printf("%n#################################################################################%n# Bienvenido a este maravilloso programa, elige una de las siguientes opciones: #%n# [1] Mostrar las String con un método                                          #%n# [2] Usar el método valueOf()                                                  #%n# [3] Usar el método equals()                                                   #%n# [4] Usar el método regionMatches()                                            #%n# [5] Concatenar cadenas usando concat()                                        #%n# [6] Ej6.2 comprar tamaño de 2 frases                                          #%n# [0] Salir                                                                     #%n#################################################################################%n");
 	}
 
 	public static void mostrarMensaje(String miCadena) {
@@ -96,6 +102,16 @@ public class U6_R1_Ej2 {
 		System.out.println("Dame una frase");
 		cad = teclado.nextLine();
 		return cad;
+	}
+
+	public static void indicarLaMasCorta(String primera, String segunda) {
+		if (primera.length() == segunda.length()) {
+			System.out.println("Ambas tienen el mismo tamaño.");
+		} else if (primera.length() > segunda.length()) {
+			System.out.println("La segunda frase es más corta.");
+		} else {
+			System.out.println("La primera frase es más corta.");
+		}
 	}
 
 }// Fin clase
