8212113a7e5c2afd3f509a2361735f092ea192eb
Danikileitor <focusyi@hotmail.com>
2023-01-13 18:49:30 +0100

    U6_R2_Ej12

--- DAW/primero/Programacion/Tema6/EjerciciosFinales/U6_R2_Ej12.java
+++ DAW/primero/Programacion/Tema6/EjerciciosFinales/U6_R2_Ej12.java
@@ -46,56 +46,12 @@ public class U6_R2_Ej12 {
 		System.out.println(miCadena);
 	}
 
-	public static void usarValueOf() {
-		String miCadena = new String("");
-		miCadena = String.valueOf(-5);
-		System.out.println("Mostramos la conversión de un entero:\t" + miCadena);
-		miCadena = String.valueOf('W');
-		System.out.println("Mostramos la conversión de un caracter:\t" + miCadena);
-		miCadena = String.valueOf(true);
-		System.out.println("Mostramos la conversión de un booleano:\t" + miCadena);
-	}
-
-	public static void comparar(String primera, String segunda) {
-		boolean valor;
-		String mensaje = "";
-		valor = primera.equals(segunda);
-		mensaje = String.valueOf(valor);
-		System.out.println("Primera cadena: " + primera + "\nSegunda cadena: " + segunda + "\n¿Son iguales? " + mensaje);
-		valor = primera.equalsIgnoreCase(segunda);
-		mensaje = String.valueOf(valor);
-		System.out.println("Primera cadena: " + primera + "\nSegunda cadena: " + segunda + "\n¿Son iguales? " + mensaje);
-	}
-
 	public static String pedirFrase() {
 		Scanner teclado = new Scanner(System.in);
 		String cad = "";
 		System.out.println("Dame una frase");
 		cad = teclado.nextLine();
 		return cad;
-	}
-
-	public static void buscar(String cadena) {
-		Scanner teclado = new Scanner(System.in);
-		String c;
-		int pos;
-		System.out.println("Introduce la letra o frase que deseas buscar:");
-		c = teclado.nextLine();
-		pos = cadena.indexOf(c);
-		System.out.printf("En la frase '%s', [%s] aparece por primera vez en la posición %d.%n",cadena,c,pos);
-	}
-
-	public static void buscarTodo(String cadena) {
-		Scanner teclado = new Scanner(System.in);
-		String c;
-		int pos, cantidad=0;
-		System.out.println("Introduce la letra o frase que deseas buscar:");
-		c = teclado.nextLine();
-		for(int i=0; i<cadena.lastIndexOf(c); i = pos+1){
-			pos = cadena.indexOf(c,i);
-			cantidad++;
-		}
-		System.out.printf("En la frase '%s', [%s] aparece en %d ocasiones.%n",cadena,c,cantidad);
 	}
 
 	public static void limpiar() {
