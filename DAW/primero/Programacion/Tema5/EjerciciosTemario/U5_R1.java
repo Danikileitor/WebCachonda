ceeb0724bdcb9172d51fae736ccd13ed6f42a725
Danikileitor <focusyi@hotmail.com>
2023-01-18 12:59:02 +0100

    U5_R1

---
+++ DAW/primero/Programacion/Tema5/EjerciciosTemario/U5_R1.java
@@ -0,0 +1,60 @@
+//Daniel Marcos Guerra Gómez
+import java.util.Scanner; //Importación de paquetes
+
+public class U5_R1 {
+	static Scanner teclado = new Scanner(System.in);
+	public static void main(String[] args) {
+		// Declarar variables
+		int menu = 1;
+		char salir = 'n';
+		// Desarrollo del programa
+		do {
+			if (menu == 0) {// Confirmación para salir del programa
+				System.out.println("Has elegido salir, ¿estas seguro? (s/n)");
+				salir = Character.toLowerCase(teclado.next().charAt(0));
+				menu++;
+			} else {
+				do {
+					menu();
+					menu = teclado.nextInt();
+					switch (menu) {
+						case 0:
+							continue;
+						case 1:
+							crearVectorEdades();
+							break;
+						default:
+							System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
+							break;
+					}// Fin switch
+				} while (menu != 0);
+			}
+		} while (salir == 'n');
+	}
+
+	public static void menu() {
+		System.out.println("#################################################################################");
+		System.out.println("# Bienvenido a este maravilloso programa, elige una de las siguientes opciones: #");
+		System.out.println("# [1] Crear vector para almacenar edades                                        #");
+		System.out.println("# [0] Salir                                                                     #");
+		System.out.println("#################################################################################");
+	}
+
+	public static void crearVectorEdades() {
+		int rango, edad;
+		System.out.println("Indica su rango:");
+		rango = teclado.nextInt();
+		int edades[] = new int[rango];
+		for (int i = 0; i < edades.length; i++) {
+			System.out.printf("Indica la edad del alumno [%d]:%n",i);
+			edad = teclado.nextInt();
+			edades[i] = edad;
+		}
+		System.out.println("Las edades de los alumnos son:");
+		for (int i = 0; i < edades.length; i++) {
+			System.out.printf("[%d] ",edades[i]);
+		}
+		System.out.println("\n");
+	}
+
+}// Fin clase
