e7454746d181e089d5a6ae5d3f1ece0b3c38bbcf
Danikileitor <focusyi@hotmail.com>
2023-01-19 08:27:56 +0100

    Plantilla

---
+++ DAW/primero/Programacion/Plantilla.java
@@ -0,0 +1,43 @@
+//Daniel Marcos Guerra Gómez
+import java.util.Scanner; //Importación de paquetes
+
+public class Plantilla {
+	public static void main(String[] args) {
+		// Declarar variables
+		int menu = 1;
+		char salir = 'n';
+		Scanner teclado = new Scanner(System.in);
+		// Desarrollo del programa
+		do {
+			if (menu == 0) {// Confirmación para salir del programa
+				System.out.println("Has elegido salir, ¿estas seguro? (s/n)");
+				salir = Character.toLowerCase(teclado.next().charAt(0));
+				menu++;
+			} else {
+				do {
+					menu();
+					menu = teclado.nextInt();
+					switch (menu) {
+						case 0:
+							continue;
+						case 1:
+							System.out.println("Hola mundo");
+							break;
+						default:
+							System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
+							break;
+					}// Fin switch
+				} while (menu != 0);
+			}
+		} while (salir == 'n');
+	}
+
+	public static void menu() {
+		System.out.println("#################################################################################");
+		System.out.println("# Bienvenido a este maravilloso programa, elige una de las siguientes opciones: #");
+		System.out.println("# [1] Hola mundo                                                                #");
+		System.out.println("# [0] Salir                                                                     #");
+		System.out.println("#################################################################################");
+	}
+
+}// Fin clase
