2fffe549bc97ea22bda935ff54c8bd8dfbff8b00
Danikileitor <focusyi@hotmail.com>
2023-02-13 15:04:45 +0100

    Ej19 + cuadrado

--- DAW/primero/Programacion/Tema5/EjerciciosFinales/U5_R2_Ej19.java
+++ DAW/primero/Programacion/Tema5/EjerciciosFinales/U5_R2_Ej19.java
@@ -1,5 +1,5 @@
 //Daniel Marcos Guerra Gómez
-//Aparte añadir un segundo ejercicio con un array de N-dimensiones que sea un cuadrado cuya diagonal sea 1 y el resto de casillas random (2-20)
+//Aparte añadir un segundo ejercicio con un array bidimensional que sea un cuadrado cuya diagonal sea 1 y el resto de casillas random (2-20)
 import java.util.Scanner; //Importación de paquetes
 
 public class U5_R2_Ej19 {
@@ -28,7 +28,7 @@ public class U5_R2_Ej19 {
 							carrera();
 							break;
 						case 2:
-							System.out.println("Hola mundo");
+							cuadrado();
 							break;
 						default:
 							System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
@@ -43,7 +43,7 @@ public class U5_R2_Ej19 {
 		System.out.println("#################################################################################");
 		System.out.println("# Bienvenido a este maravilloso programa, elige una de las siguientes opciones: #");
 		System.out.println("# [1] Carrera San Silvestre                                                     #");
-		System.out.println("# [2] Generar un array N-dimensional cuadrada                                   #");
+		System.out.println("# [2] Generar un array bidimensional con forma cuadrada                         #");
 		System.out.println("# [0] Salir                                                                     #");
 		System.out.println("#################################################################################");
 	}
@@ -69,9 +69,9 @@ public class U5_R2_Ej19 {
 			}
 			dorsales += dorsal + " ";
 		} while (dorsal != -1);
-		//Generamos la tabla de clasificación:
+		// Generamos la tabla de clasificación:
 		clasificacion = dorsales.split(" ");
-		//Comprobamos edad:
+		// Comprobamos edad:
 		System.out.println("Introduca los dorsales de los participantes menores de edad, introduce [-1] para finalizar");
 		for (int i = 0; i < clasificacion.length; i++) {
 			System.out.println("Introduce el siguiente dorsal:");
@@ -81,7 +81,7 @@ public class U5_R2_Ej19 {
 			}
 			actualizarMenor(clasificacion, dorsal);
 		}
-		//Comprobar doping:
+		// Comprobar doping:
 		System.out.println("Introduca los dorsales de los participantes que hayan dado positivo en el test antidopaje, introduce [-1] para finalizar");
 		for (int i = 0; i < clasificacion.length; i++) {
 			System.out.println("Introduce el siguiente dorsal:");
@@ -91,7 +91,7 @@ public class U5_R2_Ej19 {
 			}
 			clasificacion = expulsar(clasificacion, dorsal);
 		}
-		//Comprobar moroso:
+		// Comprobar moroso:
 		System.out.println("Introduca los dorsales de los participantes que no pagaron su inscripción, introduce [-1] para finalizar");
 		for (int i = 0; i < clasificacion.length; i++) {
 			System.out.println("Introduce el siguiente dorsal:");
@@ -101,7 +101,7 @@ public class U5_R2_Ej19 {
 			}
 			clasificacion = moverImpagos(clasificacion, dorsal);
 		}
-		//Resultado
+		// Resultado
 		System.out.println("El resultado final de la clasificación es:");
 		for (int i = 0; i < clasificacion.length; i++) {
 			System.out.println(clasificacion[i]);
@@ -109,12 +109,12 @@ public class U5_R2_Ej19 {
 	}
 
 	public static void actualizarMenor(String[] clasificacion, int dorsal) {
-		String aux = "",aux2;
+		String aux = "", aux2;
 		aux += dorsal;
 		for (int i = 0; i < clasificacion.length; i++) {
 			if (clasificacion[i].equals(aux) && i != 0) {
-				aux2=clasificacion[i-1];
-				clasificacion[i-1] = aux;
+				aux2 = clasificacion[i - 1];
+				clasificacion[i - 1] = aux;
 				clasificacion[i] = aux2;
 				break;
 			}
@@ -123,13 +123,13 @@ public class U5_R2_Ej19 {
 
 	public static String[] expulsar(String[] clasificacion, int dorsal) {
 		int pos = 0;
-		String aux = "", clasificacionNueva[] = new String[clasificacion.length-1];
+		String aux = "", clasificacionNueva[] = new String[clasificacion.length - 1];
 		aux += dorsal;
 		for (int i = 0; i < clasificacionNueva.length; i++) {
 			if (clasificacion[i].equals(aux)) {
 				pos++;
 			}
-			clasificacionNueva[i] = clasificacion[i+pos];
+			clasificacionNueva[i] = clasificacion[i + pos];
 		}
 		return clasificacionNueva;
 	}
@@ -138,14 +138,45 @@ public class U5_R2_Ej19 {
 		int pos = 0;
 		String aux = "", clasificacionNueva[] = new String[clasificacion.length];
 		aux += dorsal;
-		for (int i = 0; i < clasificacionNueva.length-1; i++) {
+		for (int i = 0; i < clasificacionNueva.length - 1; i++) {
 			if (clasificacion[i].equals(aux)) {
 				pos++;
 			}
-			clasificacionNueva[i] = clasificacion[i+pos];
+			clasificacionNueva[i] = clasificacion[i + pos];
 		}
-		clasificacionNueva[clasificacionNueva.length-1] = aux;
+		clasificacionNueva[clasificacionNueva.length - 1] = aux;
 		return clasificacionNueva;
+	}
+
+	public static void cuadrado() {
+		int cuadrado[][], longitud;
+		System.out.println("Introduzca la longitud de uno de los lados del cuadrado:");
+		longitud = pedirNum();
+		cuadrado = new int[longitud][longitud];
+		generarCuadrado(cuadrado);
+		mostrarCuadrado(cuadrado);
+	}
+
+	public static void generarCuadrado(int[][] cuadrado) {
+		for (int i = 0; i < cuadrado.length; i++) {
+			for (int j = 0; j < cuadrado.length; j++) {
+				if (i == j) {
+					cuadrado[i][j] = 1;
+				} else {
+					cuadrado[i][j] = generarAleatorio(2, 20);
+				}
+			}
+		}
+	}
+	
+	public static void mostrarCuadrado(int[][] cuadrado) {
+		System.out.println("El cuadrado:");
+		for (int i = 0; i < cuadrado.length; i++) {
+			for (int j = 0; j < cuadrado.length; j++) {
+				System.out.print("\t" + cuadrado[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }// Fin clase
