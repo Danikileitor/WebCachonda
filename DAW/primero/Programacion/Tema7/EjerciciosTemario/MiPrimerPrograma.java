b709c1e4a6b8e69d16e51c770bc40856a722abff
Danikileitor <focusyi@hotmail.com>
2023-04-12 13:58:04 +0200

    MiPrimerPrograma

--- DAW/primero/Programacion/Tema7/EjerciciosTemario/MiPrimerPrograma.java
+++ DAW/primero/Programacion/Tema7/EjerciciosTemario/MiPrimerPrograma.java
@@ -38,4 +38,17 @@ class Persona {
         estatura += incremento;
         System.out.println(nombre + " ahora mide " + estatura + "m.");
     }
+
+    static void hoyEs (int dia) {
+        switch (dia) {
+            case 1: hoy = "lunes"; break;
+            case 2: hoy = "martes"; break;
+            case 3: hoy = "miércoles"; break;
+            case 4: hoy = "jueves"; break;
+            case 5: hoy = "viernes"; break;
+            case 6: hoy = "sábado"; break;
+            case 7: hoy = "domingo"; break;
+        }
+        System.out.println("Hoy es " + Persona.hoy + ".");
+    }
 }
