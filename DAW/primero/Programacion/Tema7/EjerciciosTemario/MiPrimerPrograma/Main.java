7657801e5524147c31b1cd8aa5cb807cf5a66c13
Danikileitor <focusyi@hotmail.com>
2023-04-12 12:44:23 +0200

    MiPrimerPrograma

---
+++ DAW/primero/Programacion/Tema7/EjerciciosTemario/MiPrimerPrograma/Main.java
@@ -0,0 +1,7 @@
+package DAW.primero.Programacion.Tema7.EjerciciosTemario.MiPrimerPrograma;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ DAW/primero/Programacion/Tema7/EjerciciosTemario/MiPrimerPrograma/Persona.java
@@ -0,0 +1,10 @@
+package DAW.primero.Programacion.Tema7.EjerciciosTemario.MiPrimerPrograma;
+
+public class Persona {
+
+    String nombre;
+    byte edad;
+    double estatura;
+    final String dni;
+
+}
