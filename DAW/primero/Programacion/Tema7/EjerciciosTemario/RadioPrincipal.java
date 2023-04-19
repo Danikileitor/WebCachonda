3e9a9e95fb71c228c6a42ec9ce712c37b3ad69df
Danikileitor <focusyi@hotmail.com>
2023-04-19 14:13:33 +0200

    Radio increible

--- DAW/primero/Programacion/Tema7/EjerciciosTemario/RadioPrincipal.java
+++ DAW/primero/Programacion/Tema7/EjerciciosTemario/RadioPrincipal.java
@@ -3,60 +3,64 @@ import java.util.Scanner;
 
 public class RadioPrincipal {
     static Scanner teclado = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-        menu();        
+        menu();
     }
-    
+
     public static void menu() {
-		int menu = 1;
-		char salir = 'n';
-                Radio r = new Radio();
-		do {
-			if (menu == 0) {// Confirmación para salir del programa
-				System.out.println("Has elegido salir, ¿estas seguro? (s/n)");
-				salir = Character.toLowerCase(pedirLetra());
-				menu++;
-			} else {
-				do {
-					opcionesMenu();
-					menu = pedirNum();
-					switch (menu) {
-						case 0:
-							continue;
-						case 1:
-							r.display();
-							break;
+        int menu = 1;
+        char salir = 'n';
+        Radio r = new Radio();
+        do {
+            if (menu == 0) {// Confirmación para salir del programa
+                System.out.println("Has elegido salir, ¿estas seguro? (s/n)");
+                salir = Character.toLowerCase(pedirLetra());
+                menu++;
+            } else {
+                do {
+                    opcionesMenu();
+                    menu = pedirNum();
+                    switch (menu) {
+                        case 0:
+                            continue;
+                        case 1:
+                            r.display();
+                            break;
                         case 2:
                             r.up();
                             break;
                         case 3:
                             r.down();
                             break;
-						default:
-							System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
-							break;
-					}// Fin switch
-				} while (menu != 0);
-			}
-		} while (salir != 's');
-	}
+                        case 4:
+                            comprobarRadios(r);
+                            break;
+                        default:
+                            System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
+                            break;
+                    }// Fin switch
+                } while (menu != 0);
+            }
+        } while (salir != 's');
+    }
 
-	public static void opcionesMenu() {
-		System.out.println("##############################################################################");
-		System.out.println("# Bienvenido a esta maravilloso radio, elige una de las siguientes opciones: #");
-		System.out.println("# [1] Mostrar frecuencia sintonizada                                         #");
+    public static void opcionesMenu() {
+        System.out.println("##############################################################################");
+        System.out.println("# Bienvenido a esta maravilloso radio, elige una de las siguientes opciones: #");
+        System.out.println("# [1] Mostrar frecuencia sintonizada                                         #");
         System.out.println("# [2] Subir frecuencia                                                       #");
         System.out.println("# [3] Bajar frecuencia                                                       #");
-		System.out.println("# [0] Salir                                                                  #");
-		System.out.println("##############################################################################");
-	}
+        System.out.println("# [4] Crear una segunda radio y comprobar si es igual a la primera           #");
+        System.out.println("# [0] Salir                                                                  #");
+        System.out.println("##############################################################################");
+    }
 
-	public static char pedirLetra() {
-		return teclado.next().charAt(0);
-	}
+    public static char pedirLetra() {
+        return teclado.next().charAt(0);
+    }
 
-	public static int pedirNum() {
+    public static int pedirNum() {
         int num;
         try {
             num = teclado.nextInt();
@@ -68,46 +72,65 @@ public class RadioPrincipal {
         teclado.nextLine();
         return num;
     }
+    
+    public static long pedirFrecuencia() {
+        long num;
+        try {
+            num = teclado.nextLong();
+        } catch (InputMismatchException e) {
+            System.out.println("¡Introduce un número entero!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        teclado.nextLine();
+        return num;
+    }
+
+    public static void comprobarRadios(Radio r1) {
+        System.out.println("Indica la frecuenca para la segunda radio:");
+        Radio r2 = new Radio(pedirFrecuencia());
+        r1.iguales(r2);
+    }
 }
 
 class Radio {
 
     double frecuencia;
-    
-    Radio(){
+
+    Radio() {
         this.frecuencia = 80;
     }
-    
-    Radio(double frecuencia){
+
+    Radio(double frecuencia) {
         this.frecuencia = frecuencia;
     }
-    
-    void up(){
+
+    void up() {
         this.frecuencia += 0.5;
         this.comprobar();
         System.out.println("Frecuencia subida.");
     }
-    
-    void down(){
+
+    void down() {
         this.frecuencia -= 0.5;
         this.comprobar();
         System.out.println("Frecuencia bajada.");
     }
 
-    void comprobar(){
+    void comprobar() {
         if (this.frecuencia < 80) {
             this.frecuencia = 108;
         }
         if (this.frecuencia > 108) {
-            this.frecuencia = 80;            
+            this.frecuencia = 80;
         }
     }
-    
-    void display(){
+
+    void display() {
         System.out.println("Frecuencia sintonizada: " + this.frecuencia + " MHz.");
     }
 
-    void iguales(Radio r){
+    void iguales(Radio r) {
         boolean iguales = this.frecuencia == r.frecuencia;
         System.out.println("Iguales: " + iguales);
     }
