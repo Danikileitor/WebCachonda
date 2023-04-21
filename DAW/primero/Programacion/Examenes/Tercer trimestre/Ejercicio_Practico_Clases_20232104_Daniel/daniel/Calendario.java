b6d48564b3c1b21a6136cde0f5fa5db419040523
Danikileitor <focusyi@hotmail.com>
2023-04-21 14:13:38 +0200

    Ejercicio_Practico_Clases

---
+++ DAW/primero/Programacion/Examenes/Tercer trimestre/Ejercicio_Practico_Clases_20232104_Daniel/daniel/Calendario.java
@@ -0,0 +1,121 @@
+package daniel;//Declaración del paquete al que pertenece esta clase
+
+public class Calendario {
+
+    //Atributos de la clase, privados para que se tengan que moficar con los set()
+    private int dia, mes, anio;
+
+    //Constructor por defecto, público para que lo pueda usar cualquiera
+    public Calendario() {
+        this.dia = 21;
+        this.mes = 4;
+        this.anio = 2023;
+    }
+
+    //Constructor con argumentos, público para que lo pueda usar cualquiera
+    public Calendario(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+        this.comprobar();//Corregimos el calendario en caso de haber introducido alguno de los atributos incorrectametne
+    }
+
+    //Los setters y getters, públicos para que lo pueda usar cualquiera
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getDia() {
+        return this.dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getMes() {
+        return this.mes;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getAnio() {
+        return this.anio;
+    }
+
+    //Métodos para incrementar el día, mes y año, públicos para que lo pueda usar cualquiera
+    public void incrementarDia() {
+        this.dia++;
+        this.comprobar();
+    }
+
+    public void incrementarMes() {
+        this.mes++;
+        this.comprobar();
+    }
+
+    public void incrementarAnio(int n) {
+        this.anio += n;
+        this.comprobar();
+    }
+
+    //Métodos para comprobar que la fecha está correcta y corregirla en caso contrario, privados para que solo se pueda acceder desde la propia clase
+    private void comprobar() {
+        comprobarDia();
+        comprobarMes();
+        comprobarAnio();
+    }
+
+    private void comprobarDia() {
+        switch (this.mes) {
+            case 2:
+                if (this.dia > 28) {
+                    this.mes++;
+                    this.dia = 1;
+                }
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                if (this.dia > 31) {
+                    this.mes++;
+                    this.dia = 1;
+                }
+                break;
+            default:
+                if (this.dia > 30) {
+                    this.mes++;
+                    this.dia = 1;
+                }
+                break;
+        }
+    }
+
+    private void comprobarMes() {
+        if (this.mes > 12) {
+            this.anio++;
+            this.mes = 1;
+        }
+    }
+
+    private void comprobarAnio() {
+        if (this.anio <= 0) {
+            this.anio = 1;
+        }
+    }
+
+    //Metódos para mostrar información y comprobar si 2 calendarios son iguales, públicos para que lo pueda usar cualquiera
+    public String mostrar() {
+        return this.dia + "/" + this.mes + "/" + this.anio;
+    }
+
+    public boolean iguales(Calendario c) {
+        return this.dia == c.dia && this.mes == c.mes && this.anio == c.anio;
+    }
+}
---
+++ DAW/primero/Programacion/Examenes/Tercer trimestre/Ejercicio_Practico_Clases_20232104_Daniel/daniel/Principal_Daniel.java
@@ -0,0 +1,71 @@
+package daniel;//Declaración del paquete al que pertenece esta clase
+
+import java.util.Scanner;//Importación de paquetes
+
+public class Principal_Daniel {
+
+    //Declaración de variables globales a la clase Principal_Daniel
+    static final String ROJO = "\u001B[31m";
+    static final String VERDE = "\u001B[32m"; //Espero que los colores sean correctos (soy daltónico)
+    static final String AZUL = "\u001B[36m";
+    static final String AMARILLO = "\u001B[33m";
+    static final String RESET = "\u001B[0m";
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Declaración de variables locales al método main
+        Calendario c1, c2;
+        //Creamos el primer calendario usando un constructor con argumentos y mostramos su información
+        System.out.println("Vamos a crear un calendario");
+        c1 = new Calendario(pedirDia(), pedirMes(), pedirAnio());
+        System.out.println("Primer calendario: " + AZUL + c1.mostrar() + RESET);
+        //Se incrementa el día del primer calendario y mostramos su información
+        c1.incrementarDia();
+        System.out.println("Se incrementa un día: " + AZUL + c1.mostrar() + RESET);
+        //Se incrementa el mes del primer calendario y mostramos su información
+        c1.incrementarMes();
+        System.out.println("Se incrementa un mes: " + AZUL + c1.mostrar() + RESET);
+        //Creamos el segundo calendario usando el constructor por defecto y mostramos su información
+        c2 = new Calendario();
+        System.out.println("Segundo calendario (hoy): " + AMARILLO + c2.mostrar() + RESET);
+        //Comprobamos si ambos calendarios son iguales y mostramos su información
+        if (c1.iguales(c2)) {
+            System.out.println(VERDE + "Ambos calendarios son iguales con fecha: " + c1.mostrar() + RESET);
+        } else {
+            System.out.println(ROJO + "Ambos calendarios son diferentes" + RESET);
+            System.out.println("\tLa fecha del primer calendario es: " + AZUL + c1.mostrar() + RESET);
+            System.out.println("\tLa fecha del segundo calendario es: " + AMARILLO + c2.mostrar() + RESET);
+        }
+
+    }
+
+    //Método para solicitar enteros
+    public static int pedirNum() {
+        int n;
+        try {
+            n = teclado.nextInt();
+        } catch (Exception e) {
+            System.out.println("¡Introduce un número entero!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        return n;
+    }
+
+    //Metodos personalizados para solicitar un día, mes y año
+    public static int pedirDia() {
+        System.out.println("Introduce el día:");
+        return pedirNum();
+    }
+
+    public static int pedirMes() {
+        System.out.println("Introduce el mes:");
+        return pedirNum();
+    }
+
+    public static int pedirAnio() {
+        System.out.println("Introduce el año:");
+        return pedirNum();
+    }
+
+}
