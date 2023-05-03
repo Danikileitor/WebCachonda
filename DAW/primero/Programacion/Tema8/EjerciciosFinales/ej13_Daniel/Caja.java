32979b9b4809b705e20863fb62f49548557ffd09
Danikileitor <focusyi@hotmail.com>
2023-05-03 12:45:48 +0200

    ej13

---
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej13_Daniel/Caja.java
@@ -0,0 +1,30 @@
+package ej13_Daniel;
+
+public class Caja {
+
+    private final int ancho, alto, fondo;
+    private final Unidad unidad;
+    private String etiqueta;
+
+    public Caja(int ancho, int alto, int fondo, Unidad unidad){
+        this.ancho = ancho;
+        this.alto = alto;
+        this.fondo = fondo;
+        this.unidad = unidad;
+    }
+
+    public enum Unidad {cm, m};
+
+    public double getVolumen() {
+        return ancho*alto*fondo;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    @Override
+    public String toString() {
+        return "Etiqueta: " + this.etiqueta + ", unidad: " + this.unidad + ", ancho: " + this.ancho + ", alto: " + this.alto + ", fondo: " + this.fondo;
+    }
+}
