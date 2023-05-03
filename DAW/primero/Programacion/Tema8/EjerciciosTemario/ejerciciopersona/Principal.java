e3a375de2f3d427716a577c2489c0a5b0db41034
Danikileitor <focusyi@hotmail.com>
2023-05-03 13:04:55 +0200

    Saludamos

--- DAW/primero/Programacion/Tema8/EjerciciosTemario/ejerciciopersona/Principal.java
+++ DAW/primero/Programacion/Tema8/EjerciciosTemario/ejerciciopersona/Principal.java
@@ -7,5 +7,8 @@ public class Principal {
         e1 = new Empleado("Ja1Ro", (byte) 19, 1.76, "12345678Z", 5);
         e2 = new Empleado("Joseca", (byte) 20, 1.78, "12345679W", 899.98);
         e3 = new Empleado("Fernando", (byte) 34, 1.74, "12345678Z", 3264.16);
+        e1.saludar();
+        e2.saludar();
+        e3.saludar();
     }
 }
