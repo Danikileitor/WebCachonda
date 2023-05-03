96e48afd731fdc2db8c6ac94da9ce31b00c05d1e
Danikileitor <focusyi@hotmail.com>
2023-05-03 12:16:49 +0200

    Piano

---
+++ DAW/primero/Programacion/Tema8/EjerciciosTemario/ej5_Piano/Principal.java
@@ -0,0 +1,72 @@
+package ej5_Piano;
+
+import java.util.InputMismatchException;
+import java.util.Scanner; //Importación de paquetes
+
+import ej5_Piano.Instrumento.Nota;
+
+public class Principal {
+	static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		menu();
+	}
+
+	public static void menu() {
+		int menu = 1;
+		char salir = 'n';
+		Piano p = new Piano();
+		do {
+			if (menu == 0) {// Confirmación para salir del programa
+				System.out.println("Has elegido salir, ¿estas seguro? (s/n)");
+				salir = Character.toLowerCase(pedirLetra());
+				menu++;
+			} else {
+				do {
+					opcionesMenu();
+					menu = pedirNum();
+					switch (menu) {
+						case 0:
+							continue;
+						case 1:
+							Piano.Nota cancion[] = {Nota.DO, Nota.SI, Nota.SOL, Nota.RE, Nota.FA};
+							for (Nota nota:cancion) {
+								p.add(nota);
+							}
+							p.interpretar();
+							break;
+						default:
+							System.out.println("¡Introduce un número que corresponda a una de las opciones del menú!");
+							break;
+					}// Fin switch
+				} while (menu != 0);
+			}
+		} while (salir != 's');
+	}
+
+	public static void opcionesMenu() {
+		System.out.println("#################################################################################");
+		System.out.println("# Bienvenido a este maravilloso programa, elige una de las siguientes opciones: #");
+		System.out.println("# [1] Reproducir melodía                                                        #");
+		System.out.println("# [0] Salir                                                                     #");
+		System.out.println("#################################################################################");
+	}
+
+	public static char pedirLetra() {
+		return teclado.next().charAt(0);
+	}
+
+	public static int pedirNum() {
+        int num;
+        try {
+            num = teclado.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("¡Introduce un número entero!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        teclado.nextLine();
+        return num;
+    }
+
+}// Fin clase
