345f86b8c40e32953168e167a079fae13ebb7fd1
Danikileitor <focusyi@hotmail.com>
2023-05-03 12:07:05 +0200

    Instrumento

---
+++ DAW/primero/Programacion/Tema8/EjerciciosTemario/ej4_Instrumento/Instrumento.java
@@ -0,0 +1,22 @@
+package ej4_Instrumento;
+
+import java.util.Arrays;
+
+public abstract class Instrumento {
+
+    public enum Nota {DO, RE, MI, FA, SOL, LA, SI};
+
+    protected Nota[] melodia;
+
+    public Instrumento() {
+        this.melodia = new Nota[0];
+    }
+
+    public void add(Nota n) {
+        melodia = Arrays.copyOf(melodia, melodia.length + 1);
+        melodia[melodia.length - 1] = n;
+    }
+
+    abstract void interpretar();
+
+}
