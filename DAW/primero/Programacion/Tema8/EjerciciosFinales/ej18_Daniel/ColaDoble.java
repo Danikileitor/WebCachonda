2d01a49c23dacff7a33a05c3c8b2c2b32262d4d6
Danikileitor <focusyi@hotmail.com>
2023-05-03 14:17:57 +0200

    ej18

--- DAW/primero/Programacion/Tema8/EjerciciosFinales/ej18_Daniel/ColaDoble.java
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej18_Daniel/ColaDoble.java
@@ -2,4 +2,15 @@ package ej18_Daniel;
 
 public class ColaDoble extends Cola {
     
+    public ColaDoble() {
+        super();
+    }
+
+    public void encolarPrincipio(Integer elemento) {
+        super.insertarPrincipio(elemento);
+    }
+
+    public Integer desencolarFinal() {
+        return super.eliminar(this.tabla.length-1);
+    }
 }
