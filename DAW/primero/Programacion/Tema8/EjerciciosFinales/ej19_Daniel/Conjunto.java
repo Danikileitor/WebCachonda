33d25fffa86d29239b6f5106311d428faf522495
Danikileitor <focusyi@hotmail.com>
2023-05-05 12:25:48 +0200

    ej19

--- DAW/primero/Programacion/Tema8/EjerciciosFinales/ej19_Daniel/Conjunto.java
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej19_Daniel/Conjunto.java
@@ -1,8 +1,48 @@
 package ej19_Daniel;
 
 public class Conjunto extends Lista {
-    
+
     public Conjunto() {
         super();
+    }
+
+    private boolean comprobarRepetido(Integer nuevo) {
+        boolean repetido = false;
+        for (int i = 0; i < tabla.length; i++) {
+            repetido = tabla[i] == nuevo;
+        }
+        return repetido;
+    }
+
+    @Override
+    public void insertarPrincipio(Integer nuevo) {
+        if (!comprobarRepetido(nuevo)) {
+            super.insertarPrincipio(nuevo);
+        }
+    }
+
+    @Override
+    public void insertarFinal(Integer nuevo) {
+        if (!comprobarRepetido(nuevo)) {
+            super.insertarFinal(nuevo);
+        }
+    }
+
+    @Override
+    public void insertarFinal(Lista otraLista) {
+        boolean repetido = false;
+        for (int i = 0; i < otraLista.tabla.length; i++) {
+            comprobarRepetido(otraLista.tabla[i]);
+        }
+        if (!repetido) {
+            super.insertarFinal(otraLista);
+        }
+    }
+
+    @Override
+    public void insertar(int posicion, Integer nuevo) {
+        if (!comprobarRepetido(nuevo)) {
+            super.insertar(posicion, nuevo);
+        }
     }
 }
