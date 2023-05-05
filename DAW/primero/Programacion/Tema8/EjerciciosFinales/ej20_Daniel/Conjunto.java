33d14c79d48e37bb9ed75ac5f7809817af11f981
Danikileitor <focusyi@hotmail.com>
2023-05-05 12:46:12 +0200

    ej20

---
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej20_Daniel/Conjunto.java
@@ -0,0 +1,52 @@
+package ej20_Daniel;
+
+public class Conjunto extends Lista {
+
+    public Conjunto() {
+        super();
+    }
+
+    private boolean comprobarRepetido(Integer nuevo) {
+        boolean repetido = false;
+        for (int i = 0; i < tabla.length; i++) {
+            repetido = tabla[i] == nuevo;
+        }
+        return repetido;
+    }
+
+    @Override
+    public void insertarPrincipio(Integer nuevo) {
+        if (!comprobarRepetido(nuevo)) {
+            super.insertarPrincipio(nuevo);
+        }
+    }
+
+    @Override
+    public void insertarFinal(Integer nuevo) {
+        if (!comprobarRepetido(nuevo)) {
+            super.insertarFinal(nuevo);
+        }
+    }
+
+    @Override
+    public void insertarFinal(Lista otraLista) {
+        boolean repetido = false;
+        for (int i = 0; i < otraLista.tabla.length; i++) {
+            comprobarRepetido(otraLista.tabla[i]);
+        }
+        if (!repetido) {
+            super.insertarFinal(otraLista);
+        }
+    }
+
+    @Override
+    public void insertar(int posicion, Integer nuevo) {
+        if (!comprobarRepetido(nuevo)) {
+            super.insertar(posicion, nuevo);
+        }
+    }
+
+    public boolean equals(Conjunto otroConjunto){
+        return super.equals(otroConjunto);
+    }
+}
