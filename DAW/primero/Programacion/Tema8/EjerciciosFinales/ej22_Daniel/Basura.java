fbf5ddafc8d1a1c469d24173083b3ba8fd96401e
Danikileitor <focusyi@hotmail.com>
2023-05-09 15:26:03 +0200

    ej22

---
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej22_Daniel/Basura.java
@@ -0,0 +1,10 @@
+package ej22_Daniel;
+
+public class Basura {
+
+    static int cantidad;
+
+    Basura() {
+        cantidad++;
+    }
+}
--- DAW/primero/Programacion/Tema8/EjerciciosFinales/ej22_Daniel/Basurero.java
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej22_Daniel/Basurero.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 
 public class Basurero {
 
-    static String a = new String();
+    static String entrada = new String();
+    static Basura basura = new Basura();
     static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args) {
         do {
             System.out.println("Presiona enter para generar basura o escribe 0 para finalizar:");
-            a = teclado.nextLine();
-        } while (!a.equals("0"));
+            entrada = teclado.nextLine();
+        } while (!entrada.equals("0"));
         
     }
 
     public static void generarBasura() {
         for (int i = 0; i < 1000; i++) {
-            a = new String();
+            basura = new Basura();
         }
     }
     
