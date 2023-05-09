957a4ef06f75ee6dcd265c3de45c1455d6c47b10
Danikileitor <focusyi@hotmail.com>
2023-05-09 15:21:58 +0200

    ej22

--- DAW/primero/Programacion/Tema8/EjerciciosFinales/ej22_Daniel/Basurero.java
+++ DAW/primero/Programacion/Tema8/EjerciciosFinales/ej22_Daniel/Basurero.java
@@ -1,9 +1,28 @@
 package ej22_Daniel;
 
+import java.util.Scanner;
+
 public class Basurero {
+
+    static String a = new String();
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        do {
+            System.out.println("Presiona enter para generar basura o escribe 0 para finalizar:");
+            a = teclado.nextLine();
+        } while (!a.equals("0"));
+        
+    }
+
+    public static void generarBasura() {
+        for (int i = 0; i < 1000; i++) {
+            a = new String();
+        }
+    }
     
     @Override
     protected void finalize() throws Throwable {
-        //Habrá que poner algo aquí supongo
+        System.out.println("Ha pasado el basurero...");
     }
 }
