5bee5bc1f1ab449d1faa71f6138b6e740ea2a58e
Danikileitor <focusyi@hotmail.com>
2023-05-10 14:03:01 +0200

    U8_R3_ConstructorCopia

--- DAW/primero/Programacion/Tema8/EjerciciosTareas/Animales_Daniel/animales/U8_R3_ConstructorCopia_Daniel.java
+++ DAW/primero/Programacion/Tema8/EjerciciosTareas/Animales_Daniel/animales/U8_R3_ConstructorCopia_Daniel.java
@@ -3,19 +3,24 @@ package animales;
 public class U8_R3_ConstructorCopia_Daniel {
     public static void main(String[] args) {
 
-        //Prueba con clone
+        // Prueba con clone
         Mamifero coneja = new Mamifero(Sexo.Hembra, Tipo.Terrestre);
         Mamifero conejaClonada = (Mamifero) coneja.clone();
 
+        System.out.println("# Prueba con .clone() #");
         System.out.println("Original:");
         System.out.println(coneja);
-        System.out.println();
+        System.out.println("Clonado:");
+        System.out.println(conejaClonada);
+
+        System.out.println("# Prueba después de modificar el clonado: #");
+        System.out.println("Original:");
+        System.out.println(coneja);
+        System.out.println("Clonado:");
         System.out.println(conejaClonada);
 
         Mamifero delfin = new Mamifero(Sexo.Macho, Tipo.Acuatico);
         Mamifero delfinCopia = new Mamifero(delfin);
-
-
 
     }
 }
