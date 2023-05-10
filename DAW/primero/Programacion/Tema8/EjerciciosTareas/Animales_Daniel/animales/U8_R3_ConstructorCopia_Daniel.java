7670e3a4a5770950e44d735d43e0bc56fb5013ae
Danikileitor <focusyi@hotmail.com>
2023-05-10 14:04:58 +0200

    U8_R3_ConstructorCopia

--- DAW/primero/Programacion/Tema8/EjerciciosTareas/Animales_Daniel/animales/U8_R3_ConstructorCopia_Daniel.java
+++ DAW/primero/Programacion/Tema8/EjerciciosTareas/Animales_Daniel/animales/U8_R3_ConstructorCopia_Daniel.java
@@ -13,14 +13,33 @@ public class U8_R3_ConstructorCopia_Daniel {
         System.out.println("Clonado:");
         System.out.println(conejaClonada);
 
+        conejaClonada.sexo = Sexo.Macho;
+        conejaClonada.tipo = Tipo.Acuatico;
+
         System.out.println("# Prueba después de modificar el clonado: #");
         System.out.println("Original:");
         System.out.println(coneja);
         System.out.println("Clonado:");
         System.out.println(conejaClonada);
 
+        // Prueba con el método constructor copia
         Mamifero delfin = new Mamifero(Sexo.Macho, Tipo.Acuatico);
         Mamifero delfinCopia = new Mamifero(delfin);
+
+        System.out.println("# Prueba con el método constructor copia #");
+        System.out.println("Original:");
+        System.out.println(delfin);
+        System.out.println("Clonado:");
+        System.out.println(delfinCopia);
+
+        delfinCopia.sexo = Sexo.Hembra;
+        delfinCopia.tipo = Tipo.Terrestre;
+
+        System.out.println("# Prueba después de modificar la copia: #");
+        System.out.println("Original:");
+        System.out.println(delfin);
+        System.out.println("Clonado:");
+        System.out.println(delfinCopia);
 
     }
 }
