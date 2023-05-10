e120f80ec71aeb7cd433415af6a13a7437636121
Danikileitor <focusyi@hotmail.com>
2023-05-10 13:40:07 +0200

    Prueba2 ArrayList

--- DAW/primero/Programacion/Tema8/EjerciciosTareas/Animales_Daniel/animales/Prueba2.java
+++ DAW/primero/Programacion/Tema8/EjerciciosTareas/Animales_Daniel/animales/Prueba2.java
@@ -27,7 +27,7 @@ public class Prueba2 {
         nombres.remove(2);      //Borra el objeto según su posición
         nombres.remove("Fito"); //Borra el primero objeto indicado
 
-        ArrayList nombresCopia = (ArrayList) nombres.clone();   //Ni puta idea de como clonar
+        ArrayList nombresCopia = (ArrayList) nombres.clone();   //Clonar una lista
 
         Object[] array = nombres.toArray();     //Convertir a array normal
 
