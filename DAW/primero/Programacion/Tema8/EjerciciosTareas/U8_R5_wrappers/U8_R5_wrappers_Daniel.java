d1e49d72ce463064f157ce6a43d593c9bcd0712c
Danikileitor <focusyi@hotmail.com>
2023-05-17 12:18:54 +0200

    U8_R5_wrappers

---
+++ DAW/primero/Programacion/Tema8/EjerciciosTareas/U8_R5_wrappers/U8_R5_wrappers_Daniel.java
@@ -0,0 +1,15 @@
+public class U8_R5_wrappers_Daniel {
+
+    public static void main(String[] args) {
+
+        //Convertir una String a un entero (int) y mostrarlo
+        String s1 = "4";
+        int n1 = Integer.parseInt(s1);
+        System.out.println(n1);
+
+        //Convertir un entero (int) a una String y mostrarlo
+        int n2 = 7;
+        String s2 = String.valueOf(n2);
+        System.out.println(s2);
+    }
+}
