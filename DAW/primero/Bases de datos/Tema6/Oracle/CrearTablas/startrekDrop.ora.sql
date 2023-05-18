0f6cb9a1bd6cca873f427e0b05a6dcd3a9358a68
Danikileitor <focusyi@hotmail.com>
2023-05-18 09:10:40 +0200

    startrek

---
+++ DAW/primero/Bases de datos/Tema6/Oracle/CrearTablas/startrekDrop.ora.sql
@@ -0,0 +1,16 @@
+CONNECT startrek/startrek;
+
+DROP TABLE Visitas;
+DROP TABLE Planetas;
+DROP TABLE Lanzaderas;
+DROP TABLE Naves;
+DROP TABLE Participa;
+DROP TABLE Capitulos;
+DROP TABLE Aparece;
+DROP TABLE Peliculas;
+DROP TABLE Actores;
+DROP TABLE Depende;
+DROP TABLE Humanos;
+DROP TABLE Vulcanos;
+DROP TABLE Klingons;
+DROP TABLE Personajes;
