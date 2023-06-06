300a6a6cc4e3b676f35e6a85a1f772915c3a767a
Danikileitor <focusyi@hotmail.com>
2023-06-06 13:37:04 +0200

    ejercicio6

---
+++ DAW/primero/Programacion/Tema10/EjerciciosTareas/ejercicio6/Ejercicio6_Daniel.java
@@ -0,0 +1,46 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Ejercicio6_Daniel {
+
+    public static void main(String[] args) {
+        String texto = "";
+        FileInputStream lectura = null;
+        FileOutputStream escritura = null;
+        try {
+            lectura = new FileInputStream("C:/Users/FOCUS/Desktop/Murgi/IesMurgi.jpeg");
+            int c = lectura.read();
+            while (c != -1) {
+                texto = texto + (char) c;
+                c = lectura.read();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (lectura != null) {
+                try {
+                    lectura.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+        try {
+            escritura = new FileOutputStream("C:/Users/FOCUS/Desktop/Murgi/IesMurgi_copia.jpeg");
+            for (int i = 0; i < texto.length(); i++) {
+                escritura.write(texto.charAt(i));
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (escritura != null) {
+                try {
+                    escritura.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+}
