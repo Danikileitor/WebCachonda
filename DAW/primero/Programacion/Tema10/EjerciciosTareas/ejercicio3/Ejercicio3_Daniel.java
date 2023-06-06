a31d45071f1d24ac9999d262c8b09dd9e7e4b01a
Danikileitor <focusyi@hotmail.com>
2023-06-06 12:57:04 +0200

    ejercicio3

--- DAW/primero/Programacion/Tema10/EjerciciosTareas/ejercicio3/Ejercicio3_Daniel.java
+++ DAW/primero/Programacion/Tema10/EjerciciosTareas/ejercicio3/Ejercicio3_Daniel.java
@@ -1,4 +1,3 @@
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -6,12 +5,22 @@ import java.io.IOException;
 public class Ejercicio3_Daniel {
     
     public static void main(String[] args) {
-        BufferedReader lectura = null;
         BufferedWriter escritura = null;
         try {
-            FileWriter fichero = new FileWriter("C:/Users/FOCUS/Documents/Ficheros/salida.txt");
-            escritura = new BufferedWriter(fichero);
-            String frase = "Estamos escribiendo a través de streams de datos.";
+            FileWriter salida = new FileWriter("C:/Users/FOCUS/Documents/Ficheros/salida.txt", true); //Se añade este "true" para habilitar la sobreescritura
+            escritura = new BufferedWriter(salida);
+            String frase = "IES Murgi ( El Ejido) Almeria";
+            escritura.newLine();
+            for (int i = 0; i < frase.length(); i++) {
+                escritura.write(frase.charAt(i));
+            }
+            frase = "Curso 2021 2022.";
+            escritura.newLine();
+            for (int i = 0; i < frase.length(); i++) {
+                escritura.write(frase.charAt(i));
+            }
+            frase = "CFGS DAW.";
+            escritura.newLine();
             for (int i = 0; i < frase.length(); i++) {
                 escritura.write(frase.charAt(i));
             }
