4b864fb52162298956068203155179487105754a
Danikileitor <focusyi@hotmail.com>
2023-06-06 12:41:42 +0200

    ejercicio2

--- DAW/primero/Programacion/Tema10/EjerciciosTareas/ejercicio2/Ejercicio2_Daniel.java
+++ DAW/primero/Programacion/Tema10/EjerciciosTareas/ejercicio2/Ejercicio2_Daniel.java
@@ -5,20 +5,20 @@ import java.io.IOException;
 public class Ejercicio2_Daniel {
     
     public static void main(String[] args) {
-        BufferedWriter salida = null;
+        BufferedWriter escritura = null;
         try {
             FileWriter fichero = new FileWriter("C:/Users/FOCUS/Documents/Ficheros/salida.txt");
-            salida = new BufferedWriter(fichero);
+            escritura = new BufferedWriter(fichero);
             String frase = "Estamos escribiendo a través de streams de datos.";
             for (int i = 0; i < frase.length(); i++) {
-                salida.write(frase.charAt(i));
+                escritura.write(frase.charAt(i));
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } finally {
-            if (salida != null) {
+            if (escritura != null) {
                 try {
-                    salida.close();
+                    escritura.close();
                 } catch (IOException e) {
                     System.out.println(e);
                 }
