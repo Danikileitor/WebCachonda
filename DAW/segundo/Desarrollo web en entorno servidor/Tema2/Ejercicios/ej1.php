d0a1de396ae002b50a250964be8593b4d792319f
Danikileitor <focusyi@hotmail.com>
2023-09-22 12:50:07 +0200

    php

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/ej1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <p>
+        <?php
+            $a = strtolower("casa");
+            $b = strtolower("pasa");
+            if (substr($a, -3) === substr($b, -3)) {
+                print "Las palabras riman."
+            } else if (substr($a, -2) === substr($b, -2)) {
+                print "Las palabras riman un poco."
+            } else {
+                print "Las palabras no riman."
+            }
+        ?>
+    </p>
+</body>
+</html>
