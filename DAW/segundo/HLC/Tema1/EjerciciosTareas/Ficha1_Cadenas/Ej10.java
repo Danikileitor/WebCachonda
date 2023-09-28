651d97aef5245e74248dc0795b0e7f427709e472
Danikileitor <focusyi@hotmail.com>
2023-09-28 13:51:39 +0200

    hlc

---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha1_Cadenas/Ej10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ej10 {
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Mostramos el carácter correspondiente en ASCII entre 0 y 255:
+        for (int i = 0; i < 256; i++) {
+            System.out.print(i + ": " + (char) i + "\t");
+        }
+    }
+}
