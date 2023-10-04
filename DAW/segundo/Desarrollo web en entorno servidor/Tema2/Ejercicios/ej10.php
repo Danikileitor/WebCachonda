01c908870617294832f7b464f5417fe187210662
Danikileitor <focusyi@hotmail.com>
2023-10-04 10:27:23 +0200

    php

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/ej10.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 10</title>
+</head>
+<body>
+    <?php
+        print '<a href="/arbol/prueba.php" class="prueba" onmouseover="status=\'hola\'; return trae;">pruebade\enlace</a>';
+    ?>
+</body>
+</html>
