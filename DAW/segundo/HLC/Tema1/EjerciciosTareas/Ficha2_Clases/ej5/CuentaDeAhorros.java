ff4488881309fede37f4a9ccdf8c4adb0952685f
Danikileitor <focusyi@hotmail.com>
2023-10-06 09:29:39 +0200

    HLC

--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha2_Clases/ej5/CuentaDeAhorros.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha2_Clases/ej5/CuentaDeAhorros.java
@@ -2,5 +2,25 @@ package ej5;
 
 public class CuentaDeAhorros {
     static double tasaInteresAnual;
-    private 
+    private double saldoAhorros;
+    
+    public CuentaDeAhorros(double saldoAhorros) {
+        this.saldoAhorros = saldoAhorros;
+    }
+
+    public CuentaDeAhorros() {
+        this(0);
+    }
+
+    public double getSaldoAhorros() {
+        return saldoAhorros;
+    }
+
+    public void setSaldoAhorros(double saldoAhorros) {
+        this.saldoAhorros = saldoAhorros;
+    }
+
+    public double calcularInteresMensual() {
+        return this.getSaldoAhorros()*tasaInteresAnual/12;
+    }
 }
