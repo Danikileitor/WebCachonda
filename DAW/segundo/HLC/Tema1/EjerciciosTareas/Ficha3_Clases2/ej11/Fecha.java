9eed0ba42090d996084a128abc781738e9222363
Danikileitor <focusyi@hotmail.com>
2023-10-16 11:08:19 +0200

    HLC

--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej11/Fecha.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej11/Fecha.java
@@ -4,8 +4,16 @@ public class Fecha {
     private int dia;
     private int mes;
     private int anio;
-    
+    private String[] dias = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+    private String[] meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Juluo", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+
     public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+
+    public Fecha(String dia, int mes, int anio) {
         this.dia = dia;
         this.mes = mes;
         this.anio = anio;
