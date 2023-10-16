41acd1997006ac41b73b1fab29385201be2f0015
Danikileitor <focusyi@hotmail.com>
2023-10-16 11:00:58 +0200

    HLC

--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej11/Fecha.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej11/Fecha.java
@@ -4,4 +4,10 @@ public class Fecha {
     private int dia;
     private int mes;
     private int anio;
+    
+    public Fecha(int dia, int mes, int anio) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
 }
