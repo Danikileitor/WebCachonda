758b6275539799b0a0f31b667b81e189361d9e08
Danikileitor <focusyi@hotmail.com>
2023-10-17 13:54:29 +0200

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas3/ej3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <p>
+        <?php
+            $a = "El caballo blanco de Santiago es gris";
+            preg_match("/[aeiou]/", strtolower($a)) ? print "Tiene vocales" : print "No tiene vocales";
+        ?>
+    </p>
+</body>
+</html>
