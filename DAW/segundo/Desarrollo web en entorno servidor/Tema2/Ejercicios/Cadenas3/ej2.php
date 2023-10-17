551702088dc9e9f92b5e581a1a8abf7555a5a7a1
Danikileitor <focusyi@hotmail.com>
2023-10-17 13:42:49 +0200

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas3/ej2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <?php
+        $a = "correo@valido.com";
+        $correo = explode("@",$a);
+        if (strpos($a, "@") == true && strpos($a, ".") == true) {
+            print "<p>Correo válido</p>";
+            print "<p>Usuario: $correo[0]</p>";
+            print "<p>Dominio: $correo[1]</p>";
+        } else {
+            print "<p>Correo no válido</p>";
+        }
+    ?>
+</body>
+</html>
