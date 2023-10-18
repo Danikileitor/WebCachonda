190fae6f85e43f2ecb13a74b4e37342eebb06cf9
Danikileitor <focusyi@hotmail.com>
2023-10-18 09:33:32 +0200

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas3/ej8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 7</title>
+</head>
+<body>
+    <p>
+        <?php
+            $a = "vamos al o'Brian";
+            print $a;
+            //no hace falta escapar ningún caracter, ni siquiera el apóstrofe
+        ?>
+    </p>
+</body>
+</html>
