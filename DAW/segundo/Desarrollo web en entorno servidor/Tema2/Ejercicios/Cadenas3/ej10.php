43685535c5c1103bef4adbcbcff7bf442ab247f7
Danikileitor <focusyi@hotmail.com>
2023-10-18 09:51:07 +0200

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas3/ej10.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 9</title>
+</head>
+<body>
+    <p>
+        <?php
+            $a = "No me gusta usar +*[] en cadenas";
+            print $a;
+        ?>
+    </p>
+</body>
+</html>
