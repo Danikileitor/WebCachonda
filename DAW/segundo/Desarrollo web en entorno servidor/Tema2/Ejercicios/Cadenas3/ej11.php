0e74abd9b6fee209bf41f606b200d89b22ca19eb
Danikileitor <focusyi@hotmail.com>
2023-10-18 09:54:02 +0200

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas3/ej11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 11</title>
+</head>
+<body>
+    <p>
+        <?php
+            $a = "Esternocleidomastoideo";
+            $b = str_split($a);
+        ?>
+    </p>
+</body>
+</html>
