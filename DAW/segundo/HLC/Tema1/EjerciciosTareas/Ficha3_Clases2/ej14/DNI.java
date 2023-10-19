06806fabfca9b9b654f7af7ba8c72c32ac477a02
Danikileitor <focusyi@hotmail.com>
2023-10-19 14:46:06 +0200

    HLC DNI

--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej14/DNI.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej14/DNI.java
@@ -1,4 +1,60 @@
 package ej14;
 
 public class DNI {
+    private int numeroDni;
+
+    public DNI(int numeroDni) {
+        this.numeroDni = numeroDni;
+    }
+
+    public int getNumeroDni() {
+        return numeroDni;
+    }
+
+    public String getNIF() {
+        return Integer.toString(numeroDni) + calcularLetraNIF(numeroDni);
+    }
+
+    public void setDni(String nif) throws Exception {
+        if (validarNIF(nif)) {
+            this.numeroDni = extraerNumeroDni(nif);
+        } else {
+            throw new Exception("El NIF introducido no es válido");
+        }
+    }
+
+    public void setDni(int numeroDni) throws Exception {
+        if (String.valueOf(numeroDni).length() == 8) {
+            this.numeroDni = numeroDni;
+        } else {
+            throw new Exception("El DNI introducido no es válido");
+        }
+    }
+
+    private static char calcularLetraNIF(int numeroDni) {
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";                                                                
+        return letras.charAt(numeroDni % 23);
+    }
+
+    private static boolean validarNIF(String nif) {
+        boolean valido = false;
+        if (nif.length() == 9 && extraerLetraNIF(nif) == calcularLetraNIF(extraerNumeroDni(nif))) {
+            valido = true;
+        }
+        return valido;
+    }
+
+    private static char extraerLetraNIF(String nif) {
+        return nif.charAt(nif.length()-1);
+    }
+
+    private static int extraerNumeroDni(String nif) {
+        switch (nif.charAt(0)) {
+            case 'X': nif = '0' + nif.substring(1); break;
+            case 'Y': nif = '1' + nif.substring(1); break;
+            case 'Z': nif = '2' + nif.substring(1); break;
+            default: break;
+        }
+        return Integer.parseInt(nif.substring(0, 8));
+    }
 }
--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej14/Main.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej14/Main.java
@@ -1,6 +1,47 @@
 package ej14;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
+        DNI dni = new DNI(12345678);
+
+        System.out.println("Introduce un nuevo DNI:");
+        try {
+            dni.setDni(pedirNum());
+            System.out.println("Su DNI es: " + dni.getNumeroDni());
+            System.out.println("Su NIF es: " + dni.getNIF());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Introduce un nuevo NIF:");
+        try {
+            dni.setDni(pedirCadena());
+            System.out.println("Su DNI es: " + dni.getNumeroDni());
+            System.out.println("Su NIF es: " + dni.getNIF());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String pedirCadena() {
+		return teclado.nextLine();
+	}
+
+    public static int pedirNum() {
+        int num;
+        try {
+            num = teclado.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("¡Introduce un número entero!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        teclado.nextLine();
+        return num;
     }
 }
