cfa3da311742d865da96aea960fb1580a8e253a6
Danikileitor <focusyi@hotmail.com>
2023-10-19 13:50:09 +0200

    HLC Fecha

--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej11/Main.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha3_Clases2/ej11/Main.java
@@ -6,16 +6,22 @@ public class Main {
         Fecha fecha2 = new Fecha(25, "Abril", 1995);
         Fecha fecha3 = new Fecha(167, 1992);
 
+        System.out.println("Primera fecha:");
         fecha1.imprimirFecha1();
         fecha1.imprimirFecha2();
         fecha1.imprimirFecha3();
+        System.out.println();
 
+        System.out.println("Segunda fecha:");
         fecha2.imprimirFecha1();
         fecha2.imprimirFecha2();
         fecha2.imprimirFecha3();
+        System.out.println();
 
+        System.out.println("Tercera fecha:");
         fecha3.imprimirFecha1();
         fecha3.imprimirFecha2();
         fecha3.imprimirFecha3();
+        System.out.println();
     }
 }
