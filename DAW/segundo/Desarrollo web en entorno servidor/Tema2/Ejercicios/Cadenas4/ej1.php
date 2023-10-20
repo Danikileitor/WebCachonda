88a3567c0e9f4499175da10dd6cb77e0dfe1fde3
Danikileitor <focusyi@hotmail.com>
2023-10-20 08:35:36 +0200

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas4/ej1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <p>
+        <?php
+            $Nom = "Daniel Marcos";
+            $Apell1 = "Guerra";
+            $Apell2 = "Gómez";
+            $edad = 28;
+
+            print "<p>Me llamo $Nom $Apell1 $Apell2 y tengo $edad años</p>";
+            print "<p>Me llamo " . $Nom . $Apell1 . $Apell2 . " y tengo " . $edad . " años</p>";
+            print "<p>Me llamo $Nom\n$Apell1\n$Apell2\ny tengo $edad años</p>";
+            print "<p>Me llamo $Nom\n$Apell1\n$Apell2\ny tengo $edad años</p>";
+        ?>
+    </p>
+</body>
+</html>
