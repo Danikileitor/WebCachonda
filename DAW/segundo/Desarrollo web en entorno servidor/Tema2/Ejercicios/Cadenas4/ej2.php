79aee1cc64a0162db264c0b597e6143a1d105d9d
Danikileitor <focusyi@hotmail.com>
2023-10-27 10:29:22 +0200

    php

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas4/ej2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <p>
+        <?php
+            $a = "Esto es una cadena";
+            $b = " un poco más larga.";
+            $c = $a . $b;
+            print $c;
+        ?>
+    </p>
+</body>
+</html>
