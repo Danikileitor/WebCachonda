e831b0e1d783dd5c86113a5199704e2a44546ce4
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:07:13 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej7.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej7.js
@@ -1,11 +1,11 @@
 function ej7() {
     var a = parseFloat(prompt("Introduce un número:"));
-    if (isNaN(a)) {
+    if (isNaN(a)) {//se comprueba que se ha introducido un número
         alert("¡No has introducido un número! Inténtalo de nuevo.");
     } else {
         var b = a+5;
         var c = b+21;
         var d = c-4;
-        alert("Número introducido:\u00A0" + a + "\nContadores:\u00A0" + b + "\u00A0" + c + "\u00A0" + d);
+        alert("Número introducido:\u00A0" + a + "\nContadores:\u00A0" + b + "\u00A0" + c + "\u00A0" + d);//Utilizo \u00A0 para que mostrar mejor el espaciado entre los resultados
     }
 }
