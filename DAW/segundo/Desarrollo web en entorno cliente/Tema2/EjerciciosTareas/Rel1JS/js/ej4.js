60680c4aaf109300bcc6dd1195c66315544ed486
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:05:25 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej4.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej4.js
@@ -1,6 +1,6 @@
 function ej4() {
     var a = prompt("Introduce un número:");
-    if (isNaN(a)) {//se comprueba que edad es un número
+    if (isNaN(a)) {//se comprueba que se ha introducido un número
         alert("¡No has introducido un número! Inténtalo de nuevo.");
     } else {
         alert("Doble: " + a*2 + "\nTriple: " + a*3 + "\nCuádruple: " + a*4);//Se calcula y muestra el doble, triple y cuádruple del número introducido.
--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej6.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej6.js
@@ -1,8 +1,8 @@
 function ej6() {
     var radio = prompt("Introduce el radio:");
-    if (isNaN(radio)) {
+    if (isNaN(radio)) {//se comprueba que radio es un número
         alert("¡No has introducido un número! Inténtalo de nuevo.");
     } else {
-        alert("Longitud de la circunferencia: " + 2*Math.PI*radio + "\nÁrea del círculo: " + Math.PI*radio*radio);
+        alert("Longitud de la circunferencia: " + 2*Math.PI*radio + "\nÁrea del círculo: " + Math.PI*radio*radio);//Utilizando Math.PI podemos llamar al número PI para realizar las operaciones
     }
 }
