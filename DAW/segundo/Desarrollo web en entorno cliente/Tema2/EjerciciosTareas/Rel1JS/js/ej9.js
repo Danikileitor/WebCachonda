8c6a3b9444fa8ac20ace6e6e685d56b1b0a35031
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:08:31 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej9.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej9.js
@@ -1,8 +1,8 @@
 function ej9() {
     var a = parseInt(prompt("Introduce un número en base ocho:"));
-    if (isNaN(a)) {
+    if (isNaN(a)) {//se comprueba que se ha introducido un número
         alert("¡No has introducido un número entero! Inténtalo de nuevo.");
     } else {
-        alert("Base decimal: " + parseInt(a, 8) + "\nBase 2: " + parseInt(a, 8).toString(2));
+        alert("Base decimal: " + parseInt(a, 8) + "\nBase 2: " + parseInt(a, 8).toString(2));//Primero indicamos que el número introducido se encuentra en base 8 y la mostramos en base 2.
     }
 }
