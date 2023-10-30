02dbc62538ee643e0bfce5f67224fd5af2c941f6
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:07:55 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej8.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej8.js
@@ -1,8 +1,8 @@
 function ej8() {
     var a = parseInt(prompt("Introduce un número entero:"));
-    if (isNaN(a)) {
+    if (isNaN(a)) {//se comprueba que se ha introducido un número
         alert("¡No has introducido un número entero! Inténtalo de nuevo.");
     } else {
-        alert("Base 16: " + a.toString(16) + "\nBase 5: " + a.toString(5));
+        alert("Base 16: " + a.toString(16) + "\nBase 5: " + a.toString(5));//El método toString() permite mosrar un número cambiando su base.
     }
 }
