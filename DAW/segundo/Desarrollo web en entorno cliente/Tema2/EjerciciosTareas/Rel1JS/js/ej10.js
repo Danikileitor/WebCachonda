4dcc94ea519ca55d3f7a8d4ce4a0300a56756bbe
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:09:15 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej10.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej10.js
@@ -1,13 +1,13 @@
 function ej10() {
     var a = parseInt(prompt("Introduce un número entero:"));
     var b = parseInt(prompt("Introduce la base:"));
-    if (isNaN(a)) {
+    if (isNaN(a)) {//se comprueba que se ha introducido un número
         alert("¡No has introducido un número entero! Inténtalo de nuevo.");
     } else {
-        if (isNaN(b)) {
+        if (isNaN(b)) {//se comprueba que se ha introducido un número
             alert("¡No has introducido un número entero! Inténtalo de nuevo.");
         } else {
-            alert("El número en la base indicada es: " + a.toString(b));
+            alert("El número en la base indicada es: " + a.toString(b));//Gracias al método toString() convertimos el número 'a' a la base 'b'.
         }
     }
 }
