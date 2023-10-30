c5705f874ade910a44f918a537f7c50debc57e41
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:03:50 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej2.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej2.js
@@ -2,5 +2,5 @@ function ej2() {
     var a = prompt("Introduce un número:");
     var b = prompt("Introduce otro número:");
     var c = a/b;
-    alert("El resultado es: " + c);
+    alert("El resultado es: " + c);//Se solicitan 2 números y se muestra su división
 }
--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej3.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej3.js
@@ -1,9 +1,9 @@
 function ej3() {
     var nombre = prompt("Introduzca su nombre:");
     var edad = prompt("Introduce su edad:");
-    if (isNaN(edad)) {
+    if (isNaN(edad)) {//se comprueba que edad es un número
         alert("¡No has introducido un número! Inténtalo de nuevo.");
     } else {
-        alert("Su nombre es: " + nombre +"\nHas vivido durante " + edad*365 + " días.");
+        alert("Su nombre es: " + nombre +"\nHas vivido durante " + edad*365 + " días.");//Se calcula y muestra nombre y edad en días.
     }
 }
