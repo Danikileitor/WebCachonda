a07c6be0e81f64540a55d58941c1140cbc885d4a
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:01:54 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej1.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej1.js
@@ -1,21 +1,21 @@
 function ej1() {
     var a = prompt("Introduce un número:");
-    if (isNaN(a)) {
+    if (isNaN(a)) {//se comprueba si se ha introducido un número por teclado
         alert("¡No has introducido un número! Inténtalo de nuevo.");
     } else {
         var b = prompt("Introduce otro número:");
-        if (isNaN(b)) {
+        if (isNaN(b)) {//se comprueba si se ha introducido un número por teclado
             alert("¡No has introducido un número! Inténtalo de nuevo.");
         } else {
             var c = a-b;
-            alert("El resultado es: " + c);
+            alert("El resultado es: " + c);//en caso de que ambos datos introducidos sean números, se muestra su resta
         }
     }
 }
 
 function ej1b() {
     var a = prompt("Introduce una frase:");
-    var b = prompt("Introduce otra frase:");
+    var b = prompt("Introduce otra frase:");//Se piden 2 frases
     var c = a-b;
-    alert("El resultado es:\n" + c);
+    alert("El resultado es:\n" + c);//Se "restan" ambas frases y se mostrará NaN porque no se pueden restar frases.
 }
