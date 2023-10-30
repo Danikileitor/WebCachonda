3fbf48e5ddc76f467461d6c593198a54fba8c6dd
Danikileitor <focusyi@hotmail.com>
2023-10-30 09:04:34 +0100

    javascript r1

--- DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej4.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema2/EjerciciosTareas/Rel1JS/js/ej4.js
@@ -1,8 +1,8 @@
 function ej4() {
     var a = prompt("Introduce un número:");
-    if (isNaN(a)) {
+    if (isNaN(a)) {//se comprueba que edad es un número
         alert("¡No has introducido un número! Inténtalo de nuevo.");
     } else {
-        alert("Doble: " + a*2 + "\nTriple: " + a*3 + "\nCuádruple: " + a*4);
+        alert("Doble: " + a*2 + "\nTriple: " + a*3 + "\nCuádruple: " + a*4);//Se calcula y muestra el doble, triple y cuádruple del número introducido.
     }
 }
