471a6b69feb5b861f29b66902624f7610c8fb45e
Danikileitor <focusyi@hotmail.com>
2023-11-05 16:30:58 +0100

    HLC

---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha4_Metodos/Ej10.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ej10 {
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double[][] triangulos = { { 0, 0 }, { 0, 12 }, { 8, 15 } };
+
+        System.out.println("Las hipotenusas de los triángulos son:");
+        for (double[] triangulo : triangulos) {
+            System.out.println(hipotenusa(triangulo[0], triangulo[1]));
+        }
+    }
+
+    public static double hipotenusa(double lado1, double lado2) {
+        return Math.sqrt(lado1 * lado1 + lado2 * lado2);
+    }
+
+    public static double pedirNum() {
+        double num;
+        try {
+            num = teclado.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("¡Introduce un número!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        teclado.nextLine();
+        return num;
+    }
+}
