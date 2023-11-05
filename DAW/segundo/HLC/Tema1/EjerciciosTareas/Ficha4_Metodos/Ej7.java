300963e88fadfda38a0b60c6e549024454849e7a
Danikileitor <focusyi@hotmail.com>
2023-11-05 16:05:31 +0100

    HLC

---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha4_Metodos/Ej7.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ej7 {
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ArrayList<Double> valores = new ArrayList<>();
+        double valor;
+
+        do {
+            System.out.println("Introduce el valor número [" + valores.size() + "] (introduce 0 para finalizar)");
+            valor = pedirNum();
+            if (valor != 0) {
+                valores.add(valor);
+            }
+        } while (valor != 0);
+
+        System.out.println("Original|\tRedondeado");
+        for (double original : valores) {
+            System.out.println(original + "\t|\t" + rendoear(original));
+        }
+    }
+
+    public static double rendoear(double valor) {
+        return Math.floor(valor + 0.5);
+    }
+
+    public static double pedirNum() {
+        double num;
+        try {
+            num = teclado.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("¡Introduce un número!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        teclado.nextLine();
+        return num;
+    }
+}
