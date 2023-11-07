dfc9e9056f176ff4a0392f7d7ab083e5e59d443d
Danikileitor <focusyi@hotmail.com>
2023-11-07 10:12:01 +0100

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas4/ej7.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 7</title>
+</head>
+<body>
+    <p>
+        <?php
+            $cadena = "Bienvenido a nestro cine. Ha efectuado usted la decisión correcta.";
+            $a = "";
+            $b = "";
+            $c = "";
+            $d = "";
+            $e = "";
+            $f = "";
+            print "a. " . $a . "<br>";
+            print "a. " . $b . "<br>";
+            print "a. " . $c . "<br>";
+            print "a. " . $d . "<br>";
+            print "a. " . $e . "<br>";
+            print "a. " . $f;
+        ?>
+    </p>
+</body>
+</html>
