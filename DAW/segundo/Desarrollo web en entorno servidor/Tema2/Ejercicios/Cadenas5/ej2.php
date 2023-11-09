a86be891feeb38571b2ed9125226379e27e64486
Danikileitor <focusyi@hotmail.com>
2023-11-09 14:25:10 +0100

    PHP

--- DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas5/ej2.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas5/ej2.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 1</title>
+    <title>Ejercicio 2</title>
 </head>
 
 <body>
