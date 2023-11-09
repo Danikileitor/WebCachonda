1485fd9c200ea5a73db872941a3c8868c0488e0c
Danikileitor <focusyi@hotmail.com>
2023-11-09 14:12:40 +0100

    PHP

--- DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas5/ej1.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas5/ej1.php
@@ -10,23 +10,23 @@
 <body>
     <p>
         <?php
-        function calculaCantidad($tiempo, $dinero, $interes) {
-            if ($tiempo == 0) {
-                return $dinero;
-            } else {
-                $acumulado = $dinero + ($dinero * $tiempo / 100);
-                return calculaCantidad($tiempo - 1, $acumulado, $interes);
+            function calculaCantidad($tiempo, $dinero, $interes) {
+                if ($tiempo == 0) {
+                    return $dinero;
+                } else {
+                    $acumulado = $dinero + ($dinero * $tiempo / 100);
+                    return calculaCantidad($tiempo - 1, $acumulado, $interes);
+                }
             }
-        }
 
-        $interes = 5;
-        echo "<p><b>El interés actual es $interes%.</b></p>";
-        echo "<p>Si usted deposita 100 &#x20AC; hoy, sus ahorros crecerán a ";
-        printf("%.2f", calculaCantidad(5, 100, $interes));
-        echo "&#x20AC; en 5 años.</p>";
-        echo "<p>Si usted deposita 1.500&#x20AC; hoy, sus ahorros crecerán a ";
-        printf("%.2f", calculaCantidad(20, 1500, $interes));
-        echo "&#x20AC; después de 20 años.</p>";
+            $interes = 5;
+            echo "<p><b>El interés actual es $interes%.</b></p>";
+            echo "<p>Si usted deposita 100 &#x20AC; hoy, sus ahorros crecerán a ";
+            printf("%.2f", calculaCantidad(5, 100, $interes));
+            echo "&#x20AC; en 5 años.</p>";
+            echo "<p>Si usted deposita 1.500&#x20AC; hoy, sus ahorros crecerán a ";
+            printf("%.2f", calculaCantidad(20, 1500, $interes));
+            echo "&#x20AC; después de 20 años.</p>";
         ?>
     </p>
 </body>
