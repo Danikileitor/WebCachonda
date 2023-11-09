aea440f49e41f2246cd2726fb134b5c4af017dc0
Danikileitor <focusyi@hotmail.com>
2023-11-09 14:19:59 +0100

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas5/ej2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function infoPais($pais, $capital = "Madrid", $habitantes = "muchos") {
+            return "La capital de $pais es $capital y tiene $habitantes habitantes.";
+        }
+
+        print 
+        ?>
+    </p>
+</body>
+
+</html>
