2e493914954b8ae2c23d9933c55b97222bde7f5f
Danikileitor <focusyi@hotmail.com>
2023-11-10 08:44:59 +0100

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Cadenas5/ej6.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function negrita($texto)
+        {
+            return "<span style='font-weight: bold'>$texto</span>";
+        }
+
+        printf("La película más taquillera de la historia es %s.", negrita("Avatar de James Cameron"));
+        ?>
+    </p>
+</body>
+
+</html>
