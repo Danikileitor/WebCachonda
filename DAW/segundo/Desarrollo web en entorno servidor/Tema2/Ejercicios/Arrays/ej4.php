88faafb88e7330946f1fe8b9805e5c9d0dc0b6e7
Danikileitor <focusyi@hotmail.com>
2023-11-17 10:02:05 +0100

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Arrays/ej4.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+
+<body>
+    <?php
+    $colores = [
+        'Colores fuertes' => ['Rojo:FF0000', 'Verde:00FF00', 'Azul:0000FF'],
+        'Colores suaves' => ['Rosa:FE9ABC', 'Amarillo:FDF189', 'Malva:BC8F8F']
+    ];
+
+    print "<p>¿Se encuentra el color 'FF88CC' en el array? ";
+    if (in_array("FF88CC", $colores["Colores fuertes"]) || in_array("FF88CC", $colores["Colores suaves"])) {
+        print "Sí.</p>";
+    } else {
+        print "No.</p>";
+    }
+
+    print "<p>¿Se encuentra el color 'FF0000' en el array? ";
+    if (in_array("FF0000", $colores["Colores fuertes"]) || in_array("FF0000", $colores["Colores suaves"])) {
+        print "Sí.</p>";
+    } else {
+        print "No.</p>";
+    }
+    ?>
+</body>
+
+</html>
