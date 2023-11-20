2efc53316b7cefefaffa8bf0afa7e504175ab348
Danikileitor <focusyi@hotmail.com>
2023-11-20 10:34:08 +0100

    HLC

---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha6_Metodos3/Ej17.java
@@ -0,0 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ej17 {
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Introduce los grados farenheit como un número entero:");
+        int f = pedirNum();
+        System.out.printf("%d grados farenheit equivalen a %.2f grados centígrados.\n", f, centigrados(f));
+        System.out.println("Introduce los grados centígrados como un número entero:");
+        int c = pedirNum();
+        System.out.printf("%d grados centígrados equivalen a %.2f grados farenheit.\n", c, farenheit(c));
+    }
+
+    public static double centigrados(int f) {
+        return 5.0 / 9.0 * (f - 32);
+    }
+
+    public static double farenheit(int c) {
+        return 9.0 / 5.0 * c + 32;
+    }
+
+    public static int pedirNum() {
+        int num;
+        try {
+            num = teclado.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("¡Introduce un número entero!");
+            teclado.nextLine();
+            return pedirNum();
+        }
+        teclado.nextLine();
+        return num;
+    }
+}
