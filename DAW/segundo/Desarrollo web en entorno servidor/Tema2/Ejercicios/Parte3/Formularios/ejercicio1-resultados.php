580d72298350a316355bbca498e72e5f91ed1576
Danikileitor <focusyi@hotmail.com>
2023-11-21 14:32:44 +0100

    php

--- DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte3/Formularios/ejercicio1-resultados.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte3/Formularios/ejercicio1-resultados.php
@@ -5,14 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1 - Resultados</title>
+    <style>
+        * {
+            font-family: Arial, sans-serif;
+        }
+
+        h1 {
+            color: blue;
+        }
+    </style>
 </head>
 
 <body>
-    <p>
+    <h1>Formulario simple. Resultados del formulario</h1>
+    <p>Estos son los datos introducidos:</p>
+    <ul>
         <?php
-        //Rellenar
+        //el programita
         ?>
-    </p>
+    </ul>
+    <p>[ <a href="./ejercicio1.php">Nueva búsqueda</a> ]</p>
 </body>
 
 </html>
