d2d5a650d00703c20da35452362f1d55baad6dd9
Danikileitor <focusyi@hotmail.com>
2023-11-21 14:28:42 +0100

    php

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte3/Formularios/ejercicio1-resultados.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 - Resultados</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        //Rellenar
+        ?>
+    </p>
+</body>
+
+</html>
