300122d29c3a66dd7e5ab5f973a5fa0b5830555d
Danikileitor <focusyi@hotmail.com>
2023-11-21 12:25:53 +0100

    HLC

---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha6_Metodos3/Ej22.java
@@ -0,0 +1,54 @@
+
+//Daniel Marcos Guerra Gómez
+import java.util.Scanner;
+
+public class Ej22 {
+	static Scanner teclado = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		menu();
+	}
+
+	public static void menu() {
+		String menu = "";
+		int cara = 0, cruz = 0;
+		do {
+			opcionesMenu();
+			menu = pedirCadena();
+			if (menu.equals("salir")) {
+				break;
+			} else {
+				switch (menu) {
+					case "1":
+						if (tirar()) {
+							cruz++;
+							System.out.println("¡Cruz!");
+						} else {
+							cara++;
+							System.out.println("¡Cara!");
+						}
+						System.out.printf("Veces que ha salido cara: %d\nVeces que ha salido cruz: %d\n", cara, cruz);
+						break;
+					default:
+						System.out.println("¡Introduce una opción válida!");
+						break;
+				}
+			}
+		} while (menu != "salir");
+	}
+
+	public static void opcionesMenu() {
+		System.out.println("########################################");
+		System.out.println("# Elige una opción o escribe [salir]:  #");
+		System.out.println("# [1] Lanzar moneda                    #");
+		System.out.println("########################################");
+	}
+
+	public static String pedirCadena() {
+		return teclado.nextLine();
+	}
+
+	public static boolean tirar() {
+		return (int) (Math.random() * (2 - 1 + 1) + 1) == 1;
+	}
+}
