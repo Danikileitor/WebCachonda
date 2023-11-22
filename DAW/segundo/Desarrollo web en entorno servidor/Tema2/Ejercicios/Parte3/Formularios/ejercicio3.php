c437079979dc1e53c9d407e1a1d8421acb52170e
Danikileitor <focusyi@hotmail.com>
2023-11-22 16:06:14 +0100

    php

--- DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte3/Formularios/ejercicio3.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte3/Formularios/ejercicio3.php
@@ -31,15 +31,16 @@
 
 <body>
     <h1>Formulario simple</h1>
-    <h2>Búsqueda de canciones</h2>
     <?php
     if (isset($_REQUEST['buscar'])) {
+        echo "<h2>Resultado de la búsqueda de canciones</h2>";
         echo "<ul><li>Texto de búsqueda: " . $_REQUEST["busqueda"] . "</li>";
         echo "<li>Buscar en: " . $_REQUEST["buscaren"] . "</li>";
         echo "<li>Género: " . $_REQUEST["genero"] . "</li></ul>";
         echo "<p>[ <a href='./ejercicio3.php'>Nueva búsqueda</a> ]</p>";
     } else {
     ?>
+        <h2>Búsqueda de canciones</h2>
         <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <table>
                 <tr>
