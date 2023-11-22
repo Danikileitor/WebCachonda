88f5af22bc3e6514375785b0c866f630dec3bc86
Danikileitor <focusyi@hotmail.com>
2023-11-22 12:11:51 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTemario/ej8.js
@@ -0,0 +1,5 @@
+var perrillos = ["Rocket", "Flash", "Bella", "Slugger"];
+Console.log(perrillos.toString());
+var ciudades = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
+perrillos = ciudades.split(',');
+Console.log(perrillos.toString());
