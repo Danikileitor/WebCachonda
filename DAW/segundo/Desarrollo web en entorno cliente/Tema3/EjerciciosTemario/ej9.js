e8aa7dbfd12ac25c79f3ca6667f0b4270fdebe5c
Danikileitor <focusyi@hotmail.com>
2023-11-22 12:46:00 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTemario/ej9.js
@@ -0,0 +1,17 @@
+var arraygrande = ["28924; Estepona",
+    "28001;Madrid",
+    "08001;Barcelona",
+    "41001;Sevilla",
+    "48001;Bilbao",
+    "15001;A Coruña",
+    "46001;Valencia",
+    "29001;Málaga",
+    "03001;Alicante",
+    "07001;Palma",
+    "35001;Las Palmas de Gran Canaria",
+    "41071;Dos Hermanas",
+    "28006;Alcobendas"];
+
+function buscar() {
+    //a buscar
+}
