6c547524f6fde7ae23f835932e708f4be8ea725d
Danikileitor <focusyi@hotmail.com>
2023-11-22 12:38:10 +0100

    JS

--- DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTemario/ej8.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTemario/ej8.js
@@ -2,7 +2,7 @@ var perrillos = ["Rocket", "Flash", "Bella", "Slugger"];
 console.log(perrillos.toString());//Ambos Console.log() estaban mal, tienen que estar en mínusculas
 var ciudades = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
 perrillos = perrillos.concat(ciudades.split(','));//Unimos los nombres de los perrillos y las ciudades
-for (let i = perrillos.length - 1; i >= 0; i--) {//eliminamos los elementos que contengan C o c:
+for (let i = perrillos.length - 1; i >= 0; i--) {//Eliminamos los elementos que contengan C o c:
     if (perrillos[i].toLocaleLowerCase().includes('c')) {
         perrillos.splice(i, 1);
     }
