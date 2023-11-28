3bcae3cd98284b1946cb21ec4cc47ba8d8f1c910
Danikileitor <focusyi@hotmail.com>
2023-11-28 11:01:53 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Ventanas/js/main.js
@@ -0,0 +1,6 @@
+var parametros = "width=400, height=300, left=200, top=150 resizable=no, menubar=no, toolbar=no, directories=no, location=no, scrollbars=no, status=no";
+var ventana;
+
+function abrirVentana() {
+    ventana = window.open("texto_de_secundaria_2.htm", "ventana", parametros);
+}
