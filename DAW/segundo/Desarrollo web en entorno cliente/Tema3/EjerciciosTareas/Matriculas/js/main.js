2d925d820d49f7a7a20c565744335573f6885108
Danikileitor <focusyi@hotmail.com>
2023-11-29 16:41:01 +0100

    js

--- DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Matriculas/js/main.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Matriculas/js/main.js
@@ -8,7 +8,7 @@ function combinaciones() {  // Es posible que las combinaciones que el profesor 
     pokers = 0;         // La matrícula tiene los 4 números iguales. Ej: 8888
     var parametros = "width=400, height=300, left=200, top=150 resizable=no, menubar=no, toolbar=no, directories=no, location=no, scrollbars=no, status=no";
     ventana = window.open("combinaciones.html", "ventana", parametros);
-    ventana.onload = function () {
+    ventana.onload = function () {//Nos aseguramos de que la ventana está cargada para poder escribir en ella los resultados de las combinaciones
         var div = ventana.document.getElementById("combinaciones");
         calcularCombinaciones();
         div.innerHTML += "<ul>";
@@ -19,10 +19,11 @@ function combinaciones() {  // Es posible que las combinaciones que el profesor 
         div.innerHTML += "<li>Trío: " + trios + " (" + trios / 100 + "%)</li>"
         div.innerHTML += "</ul>";
     }
-    setTimeout("ventana.close()", 10000);
+    setTimeout("ventana.close()", 10000);//Cerramos la ventana pasados 10 segundos
 }
 
 function calcularCombinaciones() {
+    //Contamos todas las combinaciones posibles desde el 0000 hasta el 9999 y comprobamos individualmente cada una
     for (let i = 0; i <= 9999; i++) {
         doblesparejas += esDoblePareja(i) ? 1 : 0;
         trios += esTrio(i) ? 1 : 0;
@@ -38,7 +39,7 @@ function esDoblePareja(n) {
     var num = n.toString().padStart(4, '0');
     return (num.charAt(0) === num.charAt(1) && num.charAt(2) === num.charAt(3)) ||
         (num.charAt(0) === num.charAt(2) && num.charAt(1) === num.charAt(3)) ||
-        (num.charAt(0) === num.charAt(3) && num.charAt(1) === num.charAt(2));
+        (num.charAt(0) === num.charAt(3) && num.charAt(1) === num.charAt(2));//Se compara si hay 2 parejas de dígitos
 }
 
 function esTrio(n) {
@@ -46,7 +47,7 @@ function esTrio(n) {
     for (let i = 0; i < num.length; i++) {
         for (let j = i + 1; j < num.length; j++) {
             for (let k = j + 1; k < num.length; k++) {
-                if (num.charAt(i) === num.charAt(j) && num.charAt(i) === num.charAt(k)) {
+                if (num.charAt(i) === num.charAt(j) && num.charAt(i) === num.charAt(k)) {//Se compara si hay al menos 3 dígitos iguales
                     return true;
                 }
             }
@@ -57,7 +58,7 @@ function esTrio(n) {
 
 function esPoker(n) {
     var num = n.toString().padStart(4, '0');
-    if (num.charAt(0) == num.charAt(1) && num.charAt(0) == num.charAt(2) && num.charAt(0) == num.charAt(3)) {
+    if (num.charAt(0) == num.charAt(1) && num.charAt(0) == num.charAt(2) && num.charAt(0) == num.charAt(3)) {//Se compara si los 4 dígitos son iguales
         return true;
     } else {
         return false;
@@ -80,7 +81,7 @@ function esEscaleraCompleta(n) {
     var num = n.toString().padStart(4, '0');
     if ((num.charAt(3) == parseInt(num.charAt(2)) + 1 && num.charAt(2) == parseInt(num.charAt(1)) + 1 && num.charAt(1) == parseInt(num.charAt(0)) + 1) ||
         (num.charAt(3) == parseInt(num.charAt(2)) - 1 && num.charAt(2) == parseInt(num.charAt(1)) - 1 && num.charAt(1) == parseInt(num.charAt(0)) - 1)) {
-        return true;
+        return true;//Comparamos si hay una escalera de 4 dígitos consecutiva ascendente o descendente
     } else {
         return false;
     }
