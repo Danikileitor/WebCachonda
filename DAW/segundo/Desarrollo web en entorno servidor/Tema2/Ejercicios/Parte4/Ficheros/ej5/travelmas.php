f5bf3184c84af1e1ce068de75e26b4fb6d80376a
Danikileitor <focusyi@hotmail.com>
2023-12-01 18:18:59 +0100

    php

--- DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte4/Ficheros/ej5/travelmas.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema2/Ejercicios/Parte4/Ficheros/ej5/travelmas.php
@@ -1,3 +1,13 @@
+<?php
+if (isset($_REQUEST['enviar'])) {
+    if ($fichero = @fopen("viajes.txt", "a")) {
+        $linea = $_REQUEST['nombre'] . ":" . $_REQUEST['destino'] . ":" . $_REQUEST['duracion'] . ":" . $_REQUEST['dias'];
+        fwrite($fichero, "$linea\n");
+        fclose($fichero);
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
@@ -23,11 +33,53 @@
 </head>
 
 <body>
-    <h1>Formulario simple</h1>
-    <h2>Búsqueda de canciones</h2>
+    <h1>Agencia de viajes travelmas</h1>
+    <?php
+    if ($fichero = @fopen("viajes.txt", "r") && filesize("viajes.txt") > 0) {
+        $contenido = file("datos.txt");
+
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>nombre</th>";
+        echo "<th>destino</th>";
+        echo "<th>duración</th>";
+        echo "<th>salida</th>";
+        echo "</tr>";
+
+        foreach ($contenido as $linea) {
+            echo "<tr>";
+            $celdas = explode(":", $linea);
+            foreach ($celdas as $celda) {
+                echo "<td>$celda</td>";
+            }
+            echo "</td>";
+        }
+
+        echo "</table>";
+        fclose($fichero);
+    }
+    ?>
     <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <table>
-
+            <tr>
+                <td><label for="nombre">Introduzca el nombre del circuito</label></td>
+                <td><input type="text" name="nombre" id="nombre"></td>
+            </tr>
+            <tr>
+                <td><label for="destino">Introduzca el destino</label></td>
+                <td><input type="text" name="nomdestinobre" id="destino"></td>
+            </tr>
+            <tr>
+                <td><label for="duracion">Introduzca la duración</label></td>
+                <td><input type="text" name="duracion" id="duracion"></td>
+            </tr>
+            <tr>
+                <td><label for="dias">Introduzca los días de salida</label></td>
+                <td><input type="text" name="dias" id="dias"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><input type="submit" name="enviar" value="Enviar"></td>
+            </tr>
         </table>
     </form>
 </body>
