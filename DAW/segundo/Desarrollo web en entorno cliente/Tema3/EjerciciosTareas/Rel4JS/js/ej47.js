64671abd212a212351f8270381a06543df9c6366
Danikileitor <focusyi@hotmail.com>
2023-12-04 20:58:48 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej47.js
@@ -0,0 +1,15 @@
+function ej47() {
+    //Creamos un vector con 5 valores aleatorios
+    var vector = [];
+    for (let i = 0; i < 5; i++) {
+        vector.push(random(1, 1000));
+    }
+
+    //Extraemos los 2 últimos elementos y los sumamos
+    var a = vector.pop();
+    var b = vector.pop();
+    var suma = a + b;
+
+    //Mostramos la suma y el tamaño final del vector
+    alert("Suma de los 2 últimos valores: " + suma + "\nTamaño final del vector: " + vector.length);
+}
