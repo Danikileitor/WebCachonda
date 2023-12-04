93984bad03c7e72571cfc25231280595126f1325
Danikileitor <focusyi@hotmail.com>
2023-12-04 22:06:28 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej55.js
@@ -0,0 +1,18 @@
+function ej55() {
+    //Creamos el vector con 10 valores aleatorios
+    var vector = [];
+    for (let i = 0; i < 10; i++) {
+        vector.push(random(0, 100));
+    }
+
+    //Creamos 2 variables con toString y join
+    var a = vector.toString();
+    var b = vector.join();
+
+    //Mostramos un mensaje si ambas variables tienen el mismo valor
+    if (a == b) {
+        document.getElementById("55").innerHTML = "Ambas variables tienen el mismo valor";
+    }
+    
+    //No se mostrára nada porque el método toString usa la coma separador ( , ) y como no hemos indicado separadar en el método join no tendrán el mismo valor
+}
