ddaf23f0fcb9174eeac2a159282bdf484b493712
Danikileitor <focusyi@hotmail.com>
2023-12-04 21:09:10 +0100

    JS

--- DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej46.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej46.js
@@ -2,7 +2,25 @@ function ej46() {
     //Creamos el vector rifa con 1000 elementos aleatorios
     var rifa = [];
     for (let i = 0; i < 1000; i++) {
-        rifa.push(random(1, 1000));
+        rifa.push(random(1000, 10000));
     }
-    //No entiendo lo que se pide en el ejercicio
+
+    //Determinamos 10 números premiados aleatoriamente
+    var premiados = [];
+    for (let i = 0; i < 10; i++) {
+        premiados.push(rifa[random(0, rifa.length)]);
+    }
+
+    //Mostramos los números premiados y sus premios (el primero será 1000, el segundo 2000, etc)
+    alert("Los 10 números premiados y sus premios son:\n" +
+        premiados[0] + " tiene de premio: 1000\n" +
+        premiados[1] + " tiene de premio: 2000\n" +
+        premiados[2] + " tiene de premio: 3000\n" +
+        premiados[3] + " tiene de premio: 4000\n" +
+        premiados[4] + " tiene de premio: 5000\n" +
+        premiados[5] + " tiene de premio: 6000\n" +
+        premiados[6] + " tiene de premio: 7000\n" +
+        premiados[7] + " tiene de premio: 8000\n" +
+        premiados[8] + " tiene de premio: 9000\n" +
+        premiados[9] + " tiene de premio: 10000");
 }
