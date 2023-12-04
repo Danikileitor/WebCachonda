dd78ec5bb933527a6024fa6ed1c816f974268369
Danikileitor <focusyi@hotmail.com>
2023-12-04 21:52:23 +0100

    JS

--- DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej46.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej46.js
@@ -12,15 +12,15 @@ function ej46() {
     }
 
     //Mostramos los números premiados y sus premios (el primero será 1000, el segundo 2000, etc)
-    alert("Los 10 números premiados y sus premios son:\n" +
-        premiados[0] + " tiene de premio: 1000\n" +
-        premiados[1] + " tiene de premio: 2000\n" +
-        premiados[2] + " tiene de premio: 3000\n" +
-        premiados[3] + " tiene de premio: 4000\n" +
-        premiados[4] + " tiene de premio: 5000\n" +
-        premiados[5] + " tiene de premio: 6000\n" +
-        premiados[6] + " tiene de premio: 7000\n" +
-        premiados[7] + " tiene de premio: 8000\n" +
-        premiados[8] + " tiene de premio: 9000\n" +
-        premiados[9] + " tiene de premio: 10000");
+    document.getElementById("46").innerHTML = "Los 10 números premiados y sus premios son:<br>" +
+        premiados[0] + " tiene de premio: 1000<br>" +
+        premiados[1] + " tiene de premio: 2000<br>" +
+        premiados[2] + " tiene de premio: 3000<br>" +
+        premiados[3] + " tiene de premio: 4000<br>" +
+        premiados[4] + " tiene de premio: 5000<br>" +
+        premiados[5] + " tiene de premio: 6000<br>" +
+        premiados[6] + " tiene de premio: 7000<br>" +
+        premiados[7] + " tiene de premio: 8000<br>" +
+        premiados[8] + " tiene de premio: 9000<br>" +
+        premiados[9] + " tiene de premio: 10000";
 }
