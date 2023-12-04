1fa4eff3f00f430ed14ca6dc7ae9a3edc0d331ee
Danikileitor <focusyi@hotmail.com>
2023-12-04 21:32:40 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej51.js
@@ -0,0 +1,21 @@
+function ej51() {
+    //Creamos el vector con 10 valores aleatorios
+    var vector = [];
+    for (let i = 0; i < 10; i++) {
+        vector.push(random(2, 5));//Pongo este rango para que salga más fácilmente el 2 y se note el cambio por 1
+    }
+
+    //Mostramos el vector original para luego poder compararlo
+    alert(vector.toString());
+
+    //Recorremos el array y por cada elemento con valor 2 lo sustituimos por dos elementos con valor 1
+    //Lo rerorremos desde el final hacia el principio
+    for (let i = vector.length - 1; i >= 0; i--) {
+        if (vector[i] == 2) {
+            vector.splice(i, 1, 1, 1);
+        }
+    }
+
+    //Mostramos el vector final
+    alert(vector.toString());
+}
