3125b2684268df952fecd9d941e7f2c04ca49f0d
Danikileitor <focusyi@hotmail.com>
2023-12-04 20:51:42 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej46.js
@@ -0,0 +1,7 @@
+function ej46() {
+    //Creamos el vector rifa con 1000 elementos aleatorios
+    var rifa = [];
+    for (let i = 0; i < 1000; i++) {
+        rifa.push(random(1, 1000));
+    }
+}
