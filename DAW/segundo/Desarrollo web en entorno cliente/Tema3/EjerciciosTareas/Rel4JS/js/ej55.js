4bc9af5afb8bcf3d5556b19c099dcb24ca8bdc21
Danikileitor <focusyi@hotmail.com>
2023-12-04 22:10:03 +0100

    JS

--- DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej55.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej55.js
@@ -12,7 +12,10 @@ function ej55() {
     //Mostramos un mensaje si ambas variables tienen el mismo valor
     if (a == b) {
         document.getElementById("55").innerHTML = "Ambas variables tienen el mismo valor";
+    } else {
+        //No se mostrára nada porque el método toString usa la coma separador ( , ) y como no hemos indicado separadar en el método join no tendrán el mismo valor
+        //Por lo tanto mostraremos el siguiente mensaje de error:
+        document.getElementById("55").innerHTML = "Las variables no tienen el mismo valor";
     }
-    
-    //No se mostrára nada porque el método toString usa la coma separador ( , ) y como no hemos indicado separadar en el método join no tendrán el mismo valor
+
 }
