af93e0891cf8221914a416f5b660509d12bed952
Danikileitor <focusyi@hotmail.com>
2023-12-04 21:59:12 +0100

    JS

---
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema3/EjerciciosTareas/Rel4JS/js/ej53.js
@@ -0,0 +1,7 @@
+function ej53() {
+    //Creamos el vector con 5 nombres
+    var vector = ["Antonio", "Carlos", "José Luis", "Alejandro", "Eustaquio"];
+
+    //Mostramos el vector usando join con <br>
+    document.getElementById("53").innerHTML = vector.join("<br>");
+}
