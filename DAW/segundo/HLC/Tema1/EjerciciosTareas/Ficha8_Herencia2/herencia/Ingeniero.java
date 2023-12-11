987a9e5372058f1917afb60dda8a5852a9e83eb4
Danikileitor <focusyi@hotmail.com>
2023-12-11 12:32:10 +0100

    HLC

---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha8_Herencia2/herencia/Ingeniero.java
@@ -0,0 +1,11 @@
+package herencia;
+
+public class Ingeniero extends Persona {
+    public void razonar() {
+        System.out.println("*Razona su argumento*");
+    }
+
+    public void trabajarEnGrupo() {
+        System.out.println("*Se dispone a trabajar en grupo*");
+    }
+}
---
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha8_Herencia2/herencia/IngenieroInformatico.java
@@ -0,0 +1,7 @@
+package herencia;
+
+public class IngenieroInformatico extends Ingeniero {
+    public void crearPrograma() {
+        System.out.println("*Crea un programa*");
+    }
+}
