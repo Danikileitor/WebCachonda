76ce29b76577e133e8c37ff9ba5111ae3ad93fc9
Danikileitor <focusyi@hotmail.com>
2024-01-16 12:24:08 +0100

    HLC

--- DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha11_Ficheros2/Lectura.java
+++ DAW/segundo/HLC/Tema1/EjerciciosTareas/Ficha11_Ficheros2/Lectura.java
@@ -1,19 +1,33 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Lectura {
     public static void main(String[] args) {
-        // Ruta del archivo de texto (modificar antes de iniciar el programa)
-        String archivo = "archivo.txt";
+        // Ruta de los archivos de texto (modificar antes de iniciar el programa)
+        String entrada = "fichero-a.txt";
+        String salida = "fichero-b.txt";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(entrada));
+                BufferedWriter bw = new BufferedWriter(new FileWriter(salida))) {
             String linea;
+            String sinVocales;
 
-            // Leer y mostrar línea por línea
+            // Leer línea por línea
             while ((linea = br.readLine()) != null) {
-                System.out.println(linea);
+                //Quitamos las vocales
+                sinVocales = linea.replaceAll("[aáeéiíoíuúüAÁEÉIÍOÓUÚÜ]", "");
+
+                //Escribimos la línea y realizamos un salto de línea
+                bw.write(sinVocales);
+                bw.newLine();
             }
+            
+            //Informamos de que el proceso ha terminado
+            System.out.println("Se ha generado el archivo 'fichero-b.txt' con el resultado.");
+
         } catch (IOException e) {
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
