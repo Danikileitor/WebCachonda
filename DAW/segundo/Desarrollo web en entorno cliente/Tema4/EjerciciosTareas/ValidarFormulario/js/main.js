edd49371e5e4e8192e5ac7e911b10f6f28120fcf
Danikileitor <focusyi@hotmail.com>
2024-01-16 13:10:35 +0100

    JS

--- DAW/segundo/Desarrollo web en entorno cliente/Tema4/EjerciciosTareas/ValidarFormulario/js/main.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema4/EjerciciosTareas/ValidarFormulario/js/main.js
@@ -2,6 +2,7 @@
 
 const inputNombre = document.getElementById("nombre");
 const inputApellidos = document.getElementById("apellidos");
+const inputEdad = document.getElementById("edad");
 
 inputNombre.addEventListener('blur', function () {
     this.value = this.value.toUpperCase();
@@ -9,4 +10,12 @@ inputNombre.addEventListener('blur', function () {
 
 inputApellidos.addEventListener('blur', function () {
     this.value = this.value.toUpperCase();
+});
+
+inputEdad.addEventListener('blur', function () {
+    if (isNaN(this.value) || this.value < 0 || this.value > 105) {
+        alert("¡El campo edad tiene que contener un valor numérico entre 0 y 105!");
+        this.value = "";
+        this.focus();
+    }
 });
