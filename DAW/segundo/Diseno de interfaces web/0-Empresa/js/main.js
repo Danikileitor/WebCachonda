d9c1074a8fd3ceb263540a77b8134338bf173201
Danikileitor <focusyi@hotmail.com>
2024-01-21 21:15:35 +0100

    empresa video

--- DAW/segundo/Diseno de interfaces web/0-Empresa/js/main.js
+++
@@ -1,5 +0,0 @@
-(function () {
-    var url = "video/ocine_daniel_manifest.mpd";
-    var player = dashjs.MediaPlayer().create();
-    player.initialize(document.querySelector("#videoPlayer"), url, true);
-})();
