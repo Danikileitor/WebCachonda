9dbce02eff8228b9cff36eed210f6a439d33f61d
Danikileitor <focusyi@hotmail.com>
2024-01-23 13:54:17 +0100

    PHP

--- DAW/segundo/Desarrollo web en entorno servidor/Tema3/Ejercicios/Inmobiliaria/index.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema3/Ejercicios/Inmobiliaria/index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inmobiliaria</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
+
 <body>
     Hola
 </body>
+
 </html>
