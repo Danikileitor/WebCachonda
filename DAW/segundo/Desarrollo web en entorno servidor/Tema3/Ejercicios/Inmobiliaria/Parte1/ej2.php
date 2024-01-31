6cff5820f35bf006885bb6ffb447b96dea3757d0
Danikileitor <focusyi@hotmail.com>
2024-01-31 09:27:11 +0100

    PHP

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema3/Ejercicios/Inmobiliaria/Parte1/ej2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inmobiliaria - Inserción</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <h1>Inserción de nueva noticia</h1>
+    <form name="formulario" action="./ej2-resultado.php" method="post" enctype="multipart/form-data">
+        <table>
+            <tr>
+                <th>Título: *</th>
+                <td><input type="text" name="titulo"></td>
+            </tr>
+            <tr>
+                <th>Texto: *</th>
+                <td><textarea name="texto"></textarea></td>
+            </tr>
+            <tr>
+                <th>Categoría:</th>
+                <td><select name="categoria">
+                        <option value="promociones" selected>promociones</option>
+                        <option value="costas">costas</option>
+                        <option value="ofertas">ofertas</option>
+                    </select></td>
+            </tr>
+            <tr>
+                <th>Imagen:</th>
+                <td><input type="file" accept="image/*" name="imagen"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="insertar" value="Insertar noticia"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
