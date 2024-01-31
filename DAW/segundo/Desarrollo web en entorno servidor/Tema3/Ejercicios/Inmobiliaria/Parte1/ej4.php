04c7e2b40df083a93417dd5c28678b4276cfddeb
Danikileitor <focusyi@hotmail.com>
2024-01-31 10:15:12 +0100

    interfaces

--- DAW/segundo/Desarrollo web en entorno servidor/Tema3/Ejercicios/Inmobiliaria/Parte1/ej4.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema3/Ejercicios/Inmobiliaria/Parte1/ej4.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inmobiliaria - Eliminar</title>
+    <title>Inmobiliaria - Filtrar</title>
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 
 <body>
-    <h1>Eliminación de noticias</h1>
+    <h1>Consulta de noticias</h1>
     <table>
         <tr>
             <th>Título</th>
