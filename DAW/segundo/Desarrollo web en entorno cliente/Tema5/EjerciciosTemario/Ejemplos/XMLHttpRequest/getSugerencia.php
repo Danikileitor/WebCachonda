1faf510a19ace3a7124ca13a85d8f233a4c5cd2b
Danikileitor <focusyi@hotmail.com>
2024-02-12 09:51:18 +0100

    JS

--- DAW/segundo/Desarrollo web en entorno cliente/Tema5/EjerciciosTemario/Ejemplos/XMLHttpRequest/getSugerencia.php
+++ DAW/segundo/Desarrollo web en entorno cliente/Tema5/EjerciciosTemario/Ejemplos/XMLHttpRequest/getSugerencia.php
@@ -1,19 +1,19 @@
 <?php
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {  
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");  
-    header('Access-Control-Allow-Credentials: true');  
-    header('Access-Control-Max-Age: 86400');   
-}  
-  
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {  
-  
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))  
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");  
-  
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))  
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");  
-} 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+}
 
 
 
@@ -40,37 +40,28 @@ $param = $_REQUEST["param"];
 $sugerencia  = "";
 
 //Mira si coincide el parámetro con alguna de las ciudades
-if ($param !== "")
-{
+if ($param !== "") {
     //convierte el parametro recibido a minuscula
     $param = strtolower($param);
 
     //determinamos la longitud del parametro recibido
-    $len=strlen($param);
+    $len = strlen($param);
 
-    
+
     //comparamos sii el texto recibido coincide con el comienzo de alguana
     // ciudad registrada en nuestro array
-    foreach($arr as $nombre)
-    {
-        if(stristr($param, substr($nombre, 0, $len)))
-        {
-            if($sugerencia === "")
-            {
+    foreach ($arr as $nombre) {
+        if (stristr($param, substr($nombre, 0, $len))) {
+            if ($sugerencia === "") {
                 // primerasugerencia
                 $sugerencia = $nombre;
-            }
-            else
-            {
+            } else {
                 // segundas y siguientes sugerencias
                 $sugerencia .= ", $nombre";
             }
         }
-    }  
+    }
 }
 // en caso de no encontrar ninguna sugerencia, se le hace saber
 // al usuario. En caso contrario devuelve las sugerencias encontradas
 echo $sugerencia === "" ? "ninguna sugerencia" : $sugerencia;
-
-?>
-
