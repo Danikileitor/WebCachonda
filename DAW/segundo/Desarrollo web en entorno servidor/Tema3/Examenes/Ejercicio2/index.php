9a53d8a0592290710d3846c735c742a57b53a470
Danikileitor <focusyi@hotmail.com>
2024-02-23 10:41:08 +0100

    php-examenT3

--- DAW/segundo/Desarrollo web en entorno servidor/Tema3/Examenes/Ejercicio2/index.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema3/Examenes/Ejercicio2/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Examen Instituto</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="dwes.css">
 </head>
 
 <body>
