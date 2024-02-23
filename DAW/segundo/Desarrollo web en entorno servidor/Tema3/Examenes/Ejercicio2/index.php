889a17bd85230fd6e05996669bd55a4958102474
Danikileitor <focusyi@hotmail.com>
2024-02-23 10:40:46 +0100

    php-examenT3

---
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema3/Examenes/Ejercicio2/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examen Instituto</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <h1>Listado de alumnos</h1>
+    <table>
+        <tr>
+            <th>Nº Expediente</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Fecha de nacimiento</th>
+            <th>Curso</th>
+            <th>Letra</th>
+        </tr>
+        <?php
+        include_once 'fecha.php';
+        $instituto = new PDO('mysql:host=localhost;dbname=Instituto', 'instituto', 'instituto');
+        $instituto->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        try {
+            $sql = "SELECT * FROM Alumnos ORDER BY Apellidos DESC";
+            $resultado = $instituto->query($sql);
+
+            while ($alumno = $resultado->fetch(PDO::FETCH_OBJ)) {
+                $fecha = date2string($alumno->Fecha_nac);
+                echo "<tr><td>$alumno->N_expdte</td><td>$alumno->Nombre</td><td>$alumno->Apellidos</td><td>$fecha</td><td>$alumno->Curso</td><td>$alumno->Letra</td></tr>";
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        ?>
+    </table>
+</body>
+
+</html>
