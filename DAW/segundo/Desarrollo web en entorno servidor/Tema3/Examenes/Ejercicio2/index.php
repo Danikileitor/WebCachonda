3236de4339d626e047b0219995b9b10e568c6cab
Danikileitor <focusyi@hotmail.com>
2024-02-23 11:01:16 +0100

    php-examenT3

--- DAW/segundo/Desarrollo web en entorno servidor/Tema3/Examenes/Ejercicio2/index.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Tema3/Examenes/Ejercicio2/index.php
@@ -10,6 +10,21 @@
 
 <body>
     <h1>Listado de alumnos</h1>
+    <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+        <p>Filtrar por curso y letra:
+            <select name="filtroCurso">
+                <option value="todos" selected>Todos</option>
+                <option value="1">1º</option>
+                <option value="2">2º</option>
+            </select>
+            <select name="filtroLetra">
+                <option value="cualquiera" selected>Cualquiera</option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+            </select>
+            <input type="submit" name="actualizar" value="Actualizar">
+        </p>
+    </form>
     <table>
         <tr>
             <th>Nº Expediente</th>
@@ -25,8 +40,29 @@
         $instituto->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         try {
-            $sql = "SELECT * FROM Alumnos ORDER BY Apellidos DESC";
-            $resultado = $instituto->query($sql);
+            if (isset($_POST["filtroCurso"]) && $_POST["filtroCurso"] !== "todos") {
+                $filtroCurso = $_POST["filtroCurso"];
+                if (isset($_POST["filtroLetra"]) && $_POST["filtroLetra"] !== "cualquiera") {
+                    $filtroLetra = $_POST["filtroLetra"];
+                    $sql = "SELECT * FROM Alumnos WHERE Curso = ? AND Letra = ? ORDER BY Apellidos";
+                    $resultado = $instituto->prepare($sql);
+                    $resultado->execute([$filtroCurso, $filtroLetra]);
+                } else {
+                    $sql = "SELECT * FROM Alumnos WHERE Curso = ? ORDER BY Apellidos";
+                    $resultado = $instituto->prepare($sql);
+                    $resultado->execute([$filtroCurso]);
+                }
+            } else {
+                if (isset($_POST["filtroLetra"]) && $_POST["filtroLetra"] !== "cualquiera") {
+                    $filtroLetra = $_POST["filtroLetra"];
+                    $sql = "SELECT * FROM Alumnos WHERE Letra = ? ORDER BY Apellidos";
+                    $resultado = $instituto->prepare($sql);
+                    $resultado->execute([$filtroLetra]);
+                } else {
+                    $sql = "SELECT * FROM Alumnos ORDER BY Apellidos";
+                    $resultado = $instituto->query($sql);
+                }
+            }
 
             while ($alumno = $resultado->fetch(PDO::FETCH_OBJ)) {
                 $fecha = date2string($alumno->Fecha_nac);
