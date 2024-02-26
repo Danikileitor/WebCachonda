f1c1a9d26bc26976b826c72acde05ed440c1bc19
Danikileitor <focusyi@hotmail.com>
2024-02-26 09:01:31 +0100

    ProyectoTingas

--- DAW/segundo/Desarrollo web en entorno cliente/Proyecto/js/main.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Proyecto/js/main.js
@@ -1,5 +1,10 @@
 var panel = document.getElementById("login");
 
+var profesor1 = {
+    user: "gabriel",
+    pass: "1234"
+};
+
 function verlogin() {
     panel.style.display = "block";
     //setTimeout(ocultar, 60 * 1000, panel);
