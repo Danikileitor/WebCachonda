1b88ab304475b164b604f4937bdb9830795429aa
Danikileitor <focusyi@hotmail.com>
2024-03-07 14:23:32 +0100

    ProyectoTingas

--- DAW/segundo/Desarrollo web en entorno cliente/Proyecto/js/main.js
+++ DAW/segundo/Desarrollo web en entorno cliente/Proyecto/js/main.js
@@ -40,11 +40,8 @@ logeo.addEventListener('submit', submitForm);
 // Código para añadir RA y Criterios
 
 // Encuentra el botón "Añadir RA" por su ID
-var btnAddRA = document.getElementById("addRA");
-
-btnAddRA.addEventListener('click', function () {
-    // Encuentra la fila que contiene el botón "Añadir RA"
-    var rowAddRA = btnAddRA.parentNode.parentNode;
+function addRA(element) {
+    var rowAddRA = element.parentNode.parentNode;
 
     // Clona la fila de RA y la fila de criterio
     var clonedRowRA = rowAddRA.cloneNode(true);
@@ -53,41 +50,34 @@ btnAddRA.addEventListener('click', function () {
     // Inserta las filas clonadas
     rowAddRA.parentNode.insertBefore(clonedRowCriterio, rowAddRA.nextElementSibling.nextElementSibling);
     rowAddRA.parentNode.insertBefore(clonedRowRA, rowAddRA.nextElementSibling.nextElementSibling);
-});
+}
 
 // Vamos a impelemntar el botón "Quitar RA"
-var btnRemoveRA = document.getElementById("quitarRA");
-
-btnRemoveRA.addEventListener('click', function () {
+function quitarRA(element) {
     // Encuentra la fila que contiene el botón "Quitar RA"
-    var rowRemoveRA = btnRemoveRA.parentNode.parentNode;
+    var rowRemoveRA = element.parentNode.parentNode;
 
     // Elimina la fila de RA y la siguiente fila, que es la de criterio
     rowRemoveRA.nextElementSibling.parentNode.removeChild(rowRemoveRA.nextElementSibling);
     rowRemoveRA.parentNode.removeChild(rowRemoveRA);
-});
+}
 
 // Botones para los criterios
-
-var btnAddCriterio = document.getElementById("addCriterio");
-
-btnAddCriterio.addEventListener('click', function () {
+function addCriterio(element) {
     // Encuentra la fila que contiene el botón "Añadir Cr."
-    var rowAddCriterio = btnAddCriterio.parentNode.parentNode;
+    var rowAddCriterio = element.parentNode.parentNode;
 
     // Clona la fila de criterio
     var clonedRowCriterio = rowAddCriterio.cloneNode(true);
 
     // Inserta la fila clonada de criterio justo debajo de la fila de criterio original
     rowAddCriterio.parentNode.insertBefore(clonedRowCriterio, rowAddCriterio.nextElementSibling);
-});
+}
 
-var btnRemoveCriterio = document.getElementById("quitarCriterio");
-
-btnRemoveCriterio.addEventListener('click', function () {
+function quitarCriterio(element) {
     // Encuentra la fila que contiene el botón "Quitar Cr."
-    var rowRemoveCriterio = btnRemoveCriterio.parentNode.parentNode;
+    var rowRemoveCriterio = element.parentNode.parentNode;
 
     // Elimina la fila de criterio y la siguiente fila, que es la de tipo tarea
     rowRemoveCriterio.nextElementSibling.parentNode.removeChild(rowRemoveCriterio.nextElementSibling);
-});
+}
