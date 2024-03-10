4de2e7aaa000bffb7fb4a06b64cb6b3f70b496f7
Danikileitor <focusyi@hotmail.com>
2024-03-10 18:23:15 +0100

    ProyectoPHP

--- DAW/segundo/Desarrollo web en entorno servidor/Proyecto/includes/funciones.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Proyecto/includes/funciones.php
@@ -1,10 +1,15 @@
 <?php
 function cambiarTema()
 {
-    $_COOKIE["tema"] == "light" ? setcookie("tema", "dark") : setcookie("tema", "light");
+    if ($_COOKIE["tema"] == "light") {
+        setcookie("tema", "dark");
+        $_COOKIE["tema"] = "dark";
+    } else {
+        setcookie("tema", "light");
+        $_COOKIE["tema"] = "light";
+    }
 }
 if (isset($_POST["cambiarTema"])) {
     cambiarTema();
-    header("Refresh:0");
 }
 ?>
--- DAW/segundo/Desarrollo web en entorno servidor/Proyecto/index.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Proyecto/index.php
@@ -1,7 +1,7 @@
 <?php
+include_once('includes/funciones.php');
 include_once('config/config.php');
 session_start();
-include_once('config/funciones.php');
 if (!isset($_COOKIE["tema"])) {
     setcookie("tema", "light");
 }
