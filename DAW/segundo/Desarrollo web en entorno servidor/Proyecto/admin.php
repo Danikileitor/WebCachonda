f2ed978223e46a0f9b05ee9dfe5e8cb25f71559d
Danikileitor <focusyi@hotmail.com>
2024-03-10 22:10:42 +0100

    ProyectoPHP

--- DAW/segundo/Desarrollo web en entorno servidor/Proyecto/admin.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Proyecto/admin.php
@@ -27,57 +27,48 @@ if (!isset($_SESSION['usuario'])) {
         if (isset($_POST["eliminar"])) {
             if (isset($_POST["ids"])) {
                 $ids = $_POST["ids"];
-                @$dwes = new mysqli('localhost', 'dwes', 'abc123.', 'inmobiliaria');
-                $error = $dwes->connect_errno;
-                if ($error == null) {
-                    foreach ($ids as $id) {
-                        $eliminar = $dwes->stmt_init();
-                        $eliminar->prepare('DELETE FROM noticias WHERE id=?');
-                        $eliminar->bind_param('s', $id);
-                        $eliminar->execute();
-                        $eliminar->close();
-                    }
-                    $dwes->close();
-                    echo "Se han eliminado las noticias: ";
-                    foreach ($ids as $id) {
-                        echo "$id ";
-                    }
-                    echo "correctamente.";
+                foreach ($ids as $id) {
+                    $eliminar->prepare('DELETE FROM productos WHERE id=:id');
+                    $eliminar->bind_param('id', $id);
+                    $eliminar->execute();
                 }
+                echo "Se han eliminado las noticias: ";
+                foreach ($ids as $id) {
+                    echo "$id ";
+                }
+                echo "correctamente.";
             }
         }
         ?>
         <div class="container-lg d-flex flex-column min-vh-100">
             <?php include("includes/header.php"); ?>
             <main class="row gy-2 mb-2">
-                <div class="col">
-                    <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-                        <h1>Eliminación de noticias</h1>
+                <div class="col admin">
+                    <form class="borde" name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+                        <h1>Gestión de Videojuegos</h1>
                         <table>
                             <tr>
-                                <th>Título</th>
-                                <th>Texto</th>
-                                <th>Categoría</th>
-                                <th>Fecha</th>
+                                <th>ID</th>
+                                <th>Nombre</th>
                                 <th>Imagen</th>
+                                <th>Descripcion</th>
+                                <th>Precio</th>
                                 <th>Borrar</th>
                             </tr>
                             <?php
-                            $dwes = new PDO('mysql:host=localhost;dbname=inmobiliaria', 'dwes', 'abc123.');
-                            $dwes->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                             try {
-                                $sql = "SELECT titulo, texto, categoria, fecha, imagen, id FROM noticias ORDER BY fecha DESC";
-                                $resultado = $dwes->query($sql);
+                                $sql = "SELECT * FROM productos";
+                                $resultado = $connection->query($sql);
 
-                                while ($noticia = $resultado->fetch(PDO::FETCH_OBJ)) {
-                                    echo "<tr><td>$noticia->titulo</td><td>$noticia->texto</td><td>$noticia->categoria</td><td>$noticia->fecha</td><td>$noticia->imagen</td><td><input type='checkbox' name='ids[]' value='$noticia->id'></td></tr>";
+                                while ($juego = $resultado->fetch(PDO::FETCH_OBJ)) {
+                                    echo "<tr><td>$juego->id</td><td>$juego->nombre</td><td><img class='img-fluid' alt='$juego->imagen' src='" . $juego->imagen . "'></td><td>$juego->descripcion</td><td>$juego->precio</td><td><input type='checkbox' name='ids[]' value='$juego->id'></td></tr>";
                                 }
                             } catch (PDOException $e) {
                                 echo "Error: " . $e->getMessage();
                             }
                             ?>
                         </table><br>
-                        <input type="submit" name="eliminar" value="Eliminar noticias marcadas">
+                        <input type="submit" name="eliminar" value="Eliminar juegos marcados">
                     </form>
                 </div>
             </main>
