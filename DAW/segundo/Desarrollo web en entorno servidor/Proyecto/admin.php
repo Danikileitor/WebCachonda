649bec71ba8f6fdfd1a0a9ad4cfc05e12e790435
Danikileitor <focusyi@hotmail.com>
2024-03-11 14:43:27 +0100

    ProyectoPHP

--- DAW/segundo/Desarrollo web en entorno servidor/Proyecto/admin.php
+++ DAW/segundo/Desarrollo web en entorno servidor/Proyecto/admin.php
@@ -52,35 +52,21 @@ if (!isset($_SESSION['usuario'])) {
                     </nav>
                 </div>
                 <div id="contenido" class="col table-responsive">
-                    <form class="" name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-                        <h1>Gestión de Videojuegos</h1>
-                        <table class="table table-primary">
-                            <thead>
-                                <tr>
-                                    <th scope="col">ID</th>
-                                    <th scope="col">Nombre</th>
-                                    <th scope="col">Imagen</th>
-                                    <th scope="col">Descripcion</th>
-                                    <th scope="col">Precio</th>
-                                    <th scope="col">Borrar</th>
-                                </tr>
-                            </thead>
+                    <form class="" name="formulario" action="admin.php" method="post" enctype="multipart/form-data">
+                        <h1>Inserción de Videojuegos</h1>
+                        <table class="table">
                             <tbody>
-                                <?php
-                                try {
-                                    $sql = "SELECT * FROM productos";
-                                    $resultado = $connection->query($sql);
-
-                                    while ($juego = $resultado->fetch(PDO::FETCH_OBJ)) {
-                                        echo "<tr><td scope='row'>$juego->id</td><td>$juego->nombre</td><td><img class='img-fluid' alt='$juego->imagen' src='" . $juego->imagen . "'></td><td>$juego->descripcion</td><td>$juego->precio</td><td><input type='checkbox' name='ids[]' value='$juego->id'></td></tr>";
-                                    }
-                                } catch (PDOException $e) {
-                                    echo "Error: " . $e->getMessage();
-                                }
-                                ?>
+                                <tr>
+                                    <td scope="row"><label for="nombre">Nombre:</label></td>
+                                    <td><input type="text" name="nombre" id="nombre" placeholder="Nombre" required></td>
+                                </tr>
+                                <tr>
+                                    <td scope="row"><label for="imagen">Imagen:</label></td>
+                                    <td><input type="file" accept="image/*" name="imagen" id="imagen" placeholder="Nombre" required></td>
+                                </tr>
                             </tbody>
                         </table>
-                        <input type="submit" name="eliminar" value="Eliminar juegos marcados">
+                        <input type="submit" name="insertar" value="Insertar juego">
                     </form>
                 </div>
             </main>
